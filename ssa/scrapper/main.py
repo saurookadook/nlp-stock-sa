2345a74b4a36beedf1e6e996c29c9f41ba2414d2
Andrea Ovalle <andreaovalle@Andreas-MacBook-Pro.local>
2024-02-12 08:50:55 -0500

    initial scrapper testing

---
+++ ssa/scrapper/main.py
@@ -0,0 +1,13 @@
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+
+from scrapper.spiders.news_spider import NewsSpider
+
+def main():
+    process = CrawlerProcess(get_project_settings())
+    process.crawl(NewsSpider)
+    # process.crawl(MySpider2)
+    process.start()
+
+if __name__ == "__main__":
+    main()
--- ssa/scrapper/scrapper/spiders/news_spider.py
+++ ssa/scrapper/scrapper/spiders/news_spider.py
@@ -4,10 +4,14 @@ import scrapy
 
 class NewsSpider(scrapy.Spider):
     name = "news"
+    custom_settings = {
+        'USER_AGENT': 'ScarpyBot',
+        'DOWNLOAD_DELAY': 2 
+        }
 
     def start_requests(self):
         urls = [
-            "https://www.wsj.com/finance?mod=nav_top_section"
+            "https://www.wsj.com/livecoverage/stock-market-today-dow-jones-02-12-2024"
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
