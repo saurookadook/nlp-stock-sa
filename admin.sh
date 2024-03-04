b08a780eca43bcf63c4bcb72fad5faf308623aad
Andy Maskiell <maskiella@gmail.com>
2024-03-03 19:17:21 -0500

    Sorta get some basic admin scripts working

--- admin.sh
+++ admin.sh
@@ -12,11 +12,24 @@ DATABASE_NAME="the_money_maker"
 TEST_DATABASE_NAME="test_the_money_maker"
 # https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING-URIS
 PSQL_CONNECTION="postgresql://postgres:example@database"
-RETURN=$?
+LAST_RETURN_STATUS_CODE=$?
 
-isDbReady () {
-    docker-compose up -d database &  # Run in the background
-    while ! pg_isready -h database >/dev/null 2>&1; do
+
+# If you do not want that output, use...
+# -- `>/dev/null` to suppress stdout
+# -- `2>/dev/null` to suppress stderr
+# -- `&>/dev/null` to suppress both
+
+dbIsNotReady() {
+    pg_isready -h database
+    [[ $? -ne 0 ]]
+}
+
+isDbReady() {
+    # docker-compose up -d database 1> /dev/null &
+    echo "Starting ready loop..."
+    while dbIsNotReady; do
+    # while ! pg_isready -h database >/dev/null 2>&1; do
         echo "Waiting for database to be ready..."
         echo "Container ID: $(getDatabaseContainerID)"
         echo "Database Host: $(getDatabaseHost)"
@@ -24,71 +37,114 @@ isDbReady () {
     done
 }
 
-dbExists () {
-    RESULT_CODE=$(psql $PSQL_CONNECTION -l | grep -C $DATABASE_NAME)
-    echo "retcode: $RESULT_CODE"
-    [ $RESULT_CODE -eq 0 ]
+dbExists() {
+    # TODO: fix this... it doesn't work as expected
+    [ "$(psql $PSQL_CONNECTION -l | grep $DATABASE_NAME | wc -l)" -ne 0 ]
 }
 
-createDatabase () {
-    psql $PSQL_CONNECTION -c DROP DATABASE IF EXISTS $DATABASE_NAME
-    psql $PSQL_CONNECTION -f ./db/init_db.sql
+createDatabase() {
+    if [[ $* == "-d" ]]; then # drop flag included
+        echo "======================================================================================"
+        echo "Dropping $DATABASE_NAME database..."
+        echo "======================================================================================"
+        psql $PSQL_CONNECTION -c "DROP DATABASE IF EXISTS $DATABASE_NAME"
+    fi
+
+    psql $PSQL_CONNECTION -f "db/init_db.sql"
 }
 
-createTestDatabase () {
-    if drop; # drop flag included
-    then
+createTestDatabase() {
+    if [[ $* == "-d" ]]; then # drop flag included
         psql $PSQL_CONNECTION -c DROP DATABASE IF EXISTS $DATABASE_NAME
     fi
+
+    psql $PSQL_CONNECTION -f "db/init_test_db.sql"
 }
 
-initDatabase () {
+initDatabase() {
     isDbReady
 
     if dbExists; then
+        echo "======================================================================================"
         echo "$DATABASE_NAME already exists :]"
+        echo "======================================================================================"
     else
+        echo "======================================================================================"
         echo "Creating $DATABASE_NAME database..."
+        echo "======================================================================================"
         createDatabase
     fi
 }
 
-initTestDatabase () {
+initTestDatabase() {
     isDbReady
 
-    if dbExists;
-    then
+    if dbExists; then
+        echo "======================================================================================"
         echo "$TEST_DATABASE_NAME already exists :]"
+        echo "======================================================================================"
     else
+        echo "======================================================================================"
         echo "Creating $TEST_DATABASE_NAME database..."
+        echo "======================================================================================"
         createTestDatabase
     fi
 }
 
-cleanDocker () {
+cleanDocker() {
     echo "1) Prune containers"
-    docker container prune -f & fg
+    docker container prune -f # & fg
 
     echo "2) Prune images"
-    docker image prune -f & fg
+    docker image prune -f # & fg
 
     echo "3) Prune volumes"
-    docker volume prune -f & fg
+    docker volume prune -f # & fg
 
     echo "4) Prune networks"
-    docker network prune -f & fg
+    docker network prune -f # & fg
 }
 
-scriptController () {
+scriptController() {
     if [ "$1" == "db" ]; then
+        echo "======================================================================================"
+        echo "db case"
+        echo "======================================================================================"
         if [ "$2" == "init" ]; then
+            echo "======================================================================================"
             echo "Initializing $DATABASE_NAME database..."
+            echo "======================================================================================"
             initDatabase
+        elif [ "$2" == "create" ]; then
+            echo "======================================================================================"
+            echo "Creating $DATABASE_NAME database..."
+            echo "======================================================================================"
+            createDatabase
         elif [ "$2" == "init-test" ]; then
+            echo "======================================================================================"
+            echo "Initializing $TEST_DATABASE_NAME database..."
+            echo "======================================================================================"
             initTestDatabase
+        elif [ "$2" == "create-test" ]; then
+            echo "======================================================================================"
+            echo "Creating $TEST_DATABASE_NAME database..."
+            echo "======================================================================================"
+            createTestDatabase
         fi
     elif [ "$1" == "db-check" ]; then
+        echo "======================================================================================"
         echo "Checking if $DATABASE_NAME exists... $(dbExists)"
+        echo "======================================================================================"
+    elif [ "$1" == "clean" ]; then
+        echo "======================================================================================"
+        echo "clean case"
+        echo "======================================================================================"
+        if [ "$2" == "docker" ]; then
+            cleanDocker
+        fi
+    elif [ "$1" == "test" ]; then
+        echo "testing..."
+        # for testing individual things :]
     fi
 }
 
