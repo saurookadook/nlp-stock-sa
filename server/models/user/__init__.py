5bf69f5caadd3d342f0708824de4bde89bf213de
Andy Maskiell <maskiella@gmail.com>
2024-03-07 18:30:43 -0500

    Fix typo

--- server/models/user/__init__.py
+++ server/models/user/__init__.py
@@ -1,4 +1,4 @@
 from models.user.db import UserDB
 from models.user.facade import UserFacade
-from models.user.factory import UserFactory
+from models.user.factories import UserFactory
 from models.user.user import User
