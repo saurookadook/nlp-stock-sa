5713f639f0f0f363672314bdc93613c4a92a6aaa
Andy Maskiell <maskiella@gmail.com>
2024-03-11 13:28:41 -0400

    Create basic case converters

---
+++ server/nlp_ssa/utils/case_converters.py
@@ -0,0 +1,25 @@
+import re
+
+
+def convert_to_snake_case(string):
+    def replacement_func(matchObj):
+        return "_"
+
+    return re.sub(
+        pattern=r"[^\w\d]",
+        repl=replacement_func,
+        string=string,
+        flags=re.IGNORECASE | re.MULTILINE,
+    ).lower()
+
+
+def convert_to_kebab_case(string):
+    def replacement_func(matchObj):
+        return "-"
+
+    return re.sub(
+        pattern=r"[^\w\d]",
+        repl=replacement_func,
+        string=string,
+        flags=re.IGNORECASE | re.MULTILINE,
+    ).lower()
