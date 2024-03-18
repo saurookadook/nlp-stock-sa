fde19e885e36cd70e8ab87a519c77818bee865a7
Andy Maskiell <maskiella@gmail.com>
2024-03-17 22:43:08 -0400

    Create relationship between AnalysisView and User, generate migration

--- server/nlp_ssa/models/analysis_view/db.py
+++ server/nlp_ssa/models/analysis_view/db.py
@@ -1,12 +1,15 @@
 import uuid
+from sqlalchemy import ForeignKey
 from sqlalchemy.dialects import postgresql
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from db import Base
 from models.mixins import TimestampsMixin
 
 
 class AnalysisViewDB(Base, TimestampsMixin):
+    # from models.user.db import UserDB
+
     __tablename__ = "analysis_views"
 
     id: Mapped[uuid.UUID] = mapped_column(
@@ -15,9 +18,8 @@ class AnalysisViewDB(Base, TimestampsMixin):
     source_group_id: Mapped[uuid.UUID] = mapped_column(
         postgresql.UUID(as_uuid=True), nullable=False
     )
-    owner_id: Mapped[uuid.UUID] = mapped_column(
-        postgresql.UUID(as_uuid=True), nullable=False
-    )
+    owner_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("users.id"), nullable=False)
+    owner: Mapped["UserDB"] = relationship(back_populates="analysis_views")
     user_id: Mapped[uuid.UUID] = mapped_column(
         postgresql.UUID(as_uuid=True), nullable=True
     )
--- server/nlp_ssa/models/user/db.py
+++ server/nlp_ssa/models/user/db.py
@@ -1,13 +1,16 @@
 import uuid
 from sqlalchemy import String
 from sqlalchemy.dialects import postgresql
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import List
 
 from db import Base
 from models.mixins import TimestampsMixin
 
 
 class UserDB(Base, TimestampsMixin):
+    from models.analysis_view.db import AnalysisViewDB
+
     __tablename__ = "users"
 
     id: Mapped[uuid.UUID] = mapped_column(
@@ -21,3 +24,7 @@ class UserDB(Base, TimestampsMixin):
     # password: Mapped[str] = mapped_column()
     first_name: Mapped[str] = mapped_column(String(length=255), nullable=True)
     last_name: Mapped[str] = mapped_column(String(length=255), nullable=True)
+
+    analysis_views: Mapped[List["AnalysisViewDB"]] = relationship(
+        back_populates="owner"
+    )
