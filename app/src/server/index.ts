d3fec7bc8a24d45d00b0994f9611555fc8775de0
Andy Maskiell <maskiella@gmail.com>
2024-04-08 18:02:57 -0400

    Create main server script

---
+++ app/src/server/index.ts
@@ -0,0 +1,14 @@
+import expressApp from './expressApp';
+import { createTerminationHandler } from './utils';
+
+const PORT = process.env.PORT || 3001;
+const frontendServer = expressApp.listen(PORT, () => {
+    console.log(`Express app frontend server running on port ${PORT}`);
+});
+
+const exitHandler = createTerminationHandler(frontendServer);
+
+process.on('uncaughtException', exitHandler(1, 'Unexpected Error'));
+process.on('unhandledRejection', exitHandler(1, 'Unhandled Promise'));
+process.on('SIGINT', exitHandler(0, 'SIGINT'));
+process.on('SIGTERM', exitHandler(0, 'SIGTERM'));
