025856d1b4efb1c390a456a985709de741947e58
Andy Maskiell <maskiella@gmail.com>
2024-04-14 09:35:26 -0400

    Install tsconfig-paths and update lockfile, add customTSNodeLoader, clean up scripts in package.json

---
+++ app/customTSNodeLoader.js
@@ -0,0 +1,19 @@
+// from https://github.com/TypeStrong/ts-node/discussions/1450#discussioncomment-1806115
+// see also https://github.com/TypeStrong/ts-node/discussions/1450#discussion-3563207
+
+import { resolve as resolveTs } from 'ts-node/esm';
+import * as tsConfigPaths from 'tsconfig-paths';
+import { pathToFileURL } from 'url';
+
+const { absoluteBaseUrl, paths } = tsConfigPaths.loadConfig();
+console.log(''.padStart(220, '='), { absoluteBaseUrl, paths }, ''.padStart(220, '='));
+const matchPath = tsConfigPaths.createMatchPath(absoluteBaseUrl, paths);
+
+export function resolve(specifier, ctx, defaultResolve) {
+    const match = matchPath(specifier);
+    return match
+        ? resolveTs(pathToFileURL(`${match}`).href, ctx, defaultResolve)
+        : resolveTs(specifier, ctx, defaultResolve);
+}
+
+export { load, transformSource } from 'ts-node/esm';
