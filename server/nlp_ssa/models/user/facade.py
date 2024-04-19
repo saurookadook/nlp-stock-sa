54cd25012d3479a3f471063d0f2b35fd755842fd
Andy Maskiell <maskiella@gmail.com>
2024-04-19 06:05:55 -0400

    Add initial implementation for create_or_update method on UserFacade

--- server/nlp_ssa/models/user/facade.py
+++ server/nlp_ssa/models/user/facade.py
@@ -1,5 +1,8 @@
-from sqlalchemy import select
+import arrow
+from sqlalchemy import literal_column, select
+from sqlalchemy.dialects.postgresql import insert
 from sqlalchemy.orm.exc import NoResultFound
+from typing import Dict
 
 from models.user import UserDB
 from models.user.user import User
@@ -43,3 +46,21 @@ class UserFacade:
         #     raise
 
         return user_analysis_views
+
+    def create_or_update(self, *, payload: Dict) -> User:
+        # TODO: not sure this will still work?
+        insert_stmt = insert(UserDB).values(**payload)
+
+        full_stmt = insert_stmt.on_conflict_do_update(
+            constraint=UserDB.__table__.primary_key,
+            set_={
+                **payload,
+                "created_at": arrow.utcnow(),
+                "updated_at": arrow.utcnow(),
+            },
+        ).returning(literal_column("*"))
+
+        user = self.db_session.execute(full_stmt).fetchone()
+        self.db_session.flush()
+
+        return User.model_validate(user)
