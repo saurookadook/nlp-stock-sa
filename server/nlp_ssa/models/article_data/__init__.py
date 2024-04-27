b21393e98180811244500c5281e2ddd9dd6fa4f5
Andy Maskiell <maskiella@gmail.com>
2024-04-27 11:53:53 -0400

    Create initial article data entity models

---
+++ server/nlp_ssa/models/article_data/__init__.py
@@ -0,0 +1,4 @@
+from .article_data import ArticleData
+from .db import ArticleDataDB
+from .facade import ArticleDataFacade
+from .factories import ArticleDataFactory
---
+++ server/nlp_ssa/models/article_data/article_data.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, ConfigDict
+from pydantic.functional_validators import WrapValidator
+from typing import Annotated
+from uuid import UUID
+
+from utils.pydantic_helpers import convert_to_arrow_instance
+
+
+class ArticleData(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: UUID
+    quote_stock_symbol: str
+    source_group_id: UUID
+    source_url: str
+    sentence_tokens: str = ""
+    raw_content: str = ""
+    created_at: Annotated[str, WrapValidator(convert_to_arrow_instance)]
+    updated_at: Annotated[str, WrapValidator(convert_to_arrow_instance)]
