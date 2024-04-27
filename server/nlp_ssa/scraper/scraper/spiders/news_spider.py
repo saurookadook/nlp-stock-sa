fe0d45f5e04cc33ffc7189c82c1f1b18aa1779e2
Andy Maskiell <maskiella@gmail.com>
2024-04-27 13:51:13 -0400

    More fixes to yahoo news spider lol

--- server/nlp_ssa/scraper/scraper/spiders/news_spider.py
+++ server/nlp_ssa/scraper/scraper/spiders/news_spider.py
@@ -3,6 +3,7 @@ import nltk
 import re
 import scrapy
 import scrapy_splash
+import sys
 import uuid
 from bs4 import BeautifulSoup
 from nltk.tokenize import word_tokenize
@@ -14,6 +15,7 @@ from rich import inspect
 from sqlalchemy import select
 
 from nlp_ssa.db import db_session
+from nlp_ssa.models.article_data import ArticleDataFacade
 from nlp_ssa.models.stock import StockDB
 from scraper.items import ScraperItem
 
@@ -24,12 +26,15 @@ nltk.download("wordnet")
 
 class NewsSpider(scrapy.Spider):
     name = "news"
+    article_data_facade = ArticleDataFacade(db_session=db_session)
+    base_url = "https://finance.yahoo.com"
     custom_settings = {
         "USER_AGENT": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.2 Safari/605.1.15",
         "DOWNLOAD_DELAY": 2,
     }
     stock_slugs = []
-    base_url = "https://finance.yahoo.com"
+    lemmatizer = WordNetLemmatizer()
+    tokenizer = PunktSentenceTokenizer()
 
     def preprocess(self, documents):
         """
@@ -40,28 +45,6 @@ class NewsSpider(scrapy.Spider):
             preprocessed_docs.append(self.clean(doc))
         return preprocessed_docs
 
-    def clean(self, text):
-        """
-        Cleans text by:
-        - Removing URLs
-        - Converting text to lowercase
-        - Removing punctuation
-        - Removing stopwords
-        - Lemmatizing words
-        """
-        tokenizer = PunktSentenceTokenizer()
-        lemmatizer = WordNetLemmatizer()
-
-        text = re.sub(r"http\S+", "", text)  # Remove URLs
-        text = text.lower()  # Convert to lowercase
-        text = re.sub(r"[^a-zA-Z0-9]", " ", text)  # Remove punctuation
-
-        text = tokenizer.tokenize(text)
-        stops = set(stopwords.words("english"))
-        text = [word for word in text if word not in stops]  # Remove stopwords
-        text = [lemmatizer.lemmatize(word=word_1) for word_1 in text]  # Lemmatize
-        return text
-
     def start_requests(self):
         stock_symbol_slugs = db_session.execute(
             select(StockDB.quote_stock_symbol)
@@ -69,29 +52,27 @@ class NewsSpider(scrapy.Spider):
 
         self.stock_slugs.extend([result[0] for result in stock_symbol_slugs])
 
-        urls = [
-            f"https://finance.yahoo.com/quote/{slug}/news" for slug in self.stock_slugs
+        url_configs = [
+            dict(url=f"https://finance.yahoo.com/quote/{slug}/news", stock_slug=slug)
+            for slug in self.stock_slugs
         ]
-        # urls.extend(
-        #     [
-        #         f"https://finance.yahoo.com/quote/{slug}/news"
-        #         for slug in self.stock_slugs
-        #     ]
-        # )
-        inspect(urls)
+        inspect(url_configs)
 
-        for url in urls:
+        for config in url_configs:
             yield scrapy_splash.SplashRequest(
-                url=url, callback=self.follow_quote_news_links
+                url=config["url"],
+                callback=self.follow_quote_news_links,
+                cb_kwargs=dict(stock_slug=config["stock_slug"]),
             )
 
-    def follow_quote_news_links(self, response):
+    def follow_quote_news_links(self, response, stock_slug):
         news_links = self._get_non_ad_links_from_response(response)
 
         for link in news_links:
             yield scrapy_splash.SplashRequest(
                 url=link,
                 callback=self.parse,
+                cb_kwargs=dict(stock_slug=stock_slug),
                 args={
                     "wait": 2,
                     # set rendering arguments here
@@ -111,18 +92,21 @@ class NewsSpider(scrapy.Spider):
                 # "magic_response": False,  # optional, default is True
             )
 
-    def parse(self, response):
+    def parse(self, response, stock_slug):
         """
         - parse html
         - runs methods to pre-process data for nlp models
         - yields item
         """
-        self._debug_logger(header_text="NewsSpider.parse")
+        self._debug_logger(
+            header_text="NewsSpider.parse", variables=[response.url, stock_slug]
+        )
 
         article_content = response.css("div.morpheusGridBody div.caas-body").get()
         soup = BeautifulSoup(article_content, "html.parser")
-        cleaned_text = self.clean(soup.get_text())
-        item = ScraperItem()
+        raw_text = soup.get_text()
+        cleaned_text = self.clean(raw_text)
+        source_group_id = uuid.uuid4()
 
         self._debug_logger(
             header_text="NewsSpider.parse - cleaned_text",
@@ -130,9 +114,42 @@ class NewsSpider(scrapy.Spider):
             width=200,
         )
 
+        item = ScraperItem()
         item["Sentence"] = cleaned_text
-        item["GroupId"] = uuid.uuid4()
+        item["GroupId"] = source_group_id
+        try:
+            self.article_data_facade.create_or_update(
+                payload=dict(
+                    id=uuid.uuid4(),
+                    quote_stock_symbol=stock_slug,
+                    source_group_id=source_group_id,
+                    source_url=response.url,
+                    raw_content=raw_text,
+                    sentence_tokens=cleaned_text,
+                )
+            )
+        except Exception as e:
+            print(e, file=sys.stderr)
         yield item
+
+    def clean(self, text):
+        """
+        Cleans text by:
+        - Removing URLs
+        - Converting text to lowercase
+        - Removing punctuation
+        - Removing stopwords
+        - Lemmatizing words
+        """
+        text = re.sub(r"http\S+", "", text)  # Remove URLs
+        text = text.lower()  # Convert to lowercase
+        text = re.sub(r"[^a-zA-Z0-9]", " ", text)  # Remove punctuation
+
+        text = self.tokenizer.tokenize(text)
+        stops = set(stopwords.words("english"))
+        text = [word for word in text if word not in stops]  # Remove stopwords
+        text = [self.lemmatizer.lemmatize(word=word_1) for word_1 in text]  # Lemmatize
+        return text
 
     def _debug_logger(
         self, *, header_text: str, variables: list = [], width: int = 200
