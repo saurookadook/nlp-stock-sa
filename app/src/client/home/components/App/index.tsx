b028816b3c2e4e8af1625ff6422d75018cde0419
Andy Maskiell <maskiella@gmail.com>
2024-04-28 20:43:16 -0400

    Add elements for article data to basic home App component

--- app/src/client/home/components/App/index.tsx
+++ app/src/client/home/components/App/index.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import { Center, ChakraProvider, Container } from '@chakra-ui/react';
 // import logo from '/logo.svg';
 
-function App({ data }: { data?: Record<string, unknown> | null }) {
-    console.log('home - App', { data });
+interface InitialPageData {
+    data: Record<string, unknown>[] | null;
+}
+
+function App({ initialPageData }: { initialPageData: InitialPageData }) {
+    const { data: pageData } = initialPageData;
+    console.log('home - App', { initialPageData });
     return (
         <ChakraProvider>
             <Container className="home" maxWidth="75vw">
@@ -25,6 +30,26 @@ function App({ data }: { data?: Record<string, unknown> | null }) {
                             <b>{`💸 🤑 💸 THE MONEY MAKERRRRR 💸 🤑 💸 `}</b>
                         </p>
                     </div>
+                    <section>
+                        <h1>Home: Article Data</h1>
+                        {pageData != null && pageData.length > 0 ? (
+                            <ul>
+                                {pageData.map((record, i) => (
+                                    <li key={`data-record-${i}`}>
+                                        <details>
+                                            <summary>
+                                                Stock: {record.quoteStockSymbol}, URL: {record.sourceUrl}
+                                            </summary>
+                                            <div>{record.rawContent}</div>
+                                            <div>{record.sentenceTokens}</div>
+                                        </details>
+                                    </li>
+                                ))}
+                            </ul>
+                        ) : (
+                            'No data :['
+                        )}
+                    </section>
                 </Center>
             </Container>
         </ChakraProvider>
