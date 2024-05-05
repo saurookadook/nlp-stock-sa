5f8159ff2e6a292135f28799b9a28f9b1495d002
Andy Maskiell <maskiella@gmail.com>
2024-05-05 10:43:52 -0400

    Expose port in Dockerfile, create all stocks route, improve seed_db script

--- server/nlp_ssa/api/routes/api/stocks/router.py
+++ server/nlp_ssa/api/routes/api/stocks/router.py
@@ -2,6 +2,7 @@
 import logging
 from fastapi import APIRouter, Depends, Request
 from fastapi_csrf_protect import CsrfProtect
+from sqlalchemy import select
 
 from config import configure_logging
 from db import db_session
@@ -9,6 +10,15 @@ from db import db_session
 configure_logging(app_name="nlp_ssa.api.routes.stocks")
 logger = logging.getLogger(__name__)
 router = APIRouter()
+
+
+@router.get("/api/stocks/all")
+async def read_all_stocks():
+    from models.stock import StockDB
+
+    all_stocks = db_session.execute(select(StockDB)).scalars().all()
+
+    return {"data": all_stocks}
 
 
 @router.post("/api/stocks/new")
