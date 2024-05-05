775de4299b74a51792da67f4d1f2b086f6f5c8f8
Andy Maskiell <maskiella@gmail.com>
2024-05-05 13:02:35 -0400

    Minor cleanup

--- app/src/server/home/index.ts
+++ app/src/server/home/index.ts
@@ -12,20 +12,18 @@ router.use(
         let initialPageData = {};
         try {
             // TODO: add some user-specific thing to request?
-            // const pageDataResponse = await fetch(`/nlp-ssa/internal-api/v1/home`);
-            const pageDataResponse = await global.fetch('http://localhost:3000/api/article-data', { mode: 'cors' });
+            const pageDataResponse = await global.fetch('https://nlp-ssa.dev/api/article-data', { mode: 'cors' });
             initialPageData = await pageDataResponse.json();
         } catch (e) {
             console.warn(`[home route] - caught exception: ${e}`);
         }
 
-        // const { appJs: reactVendorsJs } = res.locals.manifest['react-vendors'];
-        console.log(
-            '\n'.padStart(220, '='),
-            'home router: ',
-            { localsManifest: res.locals.manifest },
-            '\n'.padEnd(220, '='),
-        );
+        // console.log(
+        //     '\n'.padStart(220, '='),
+        //     'home router: ',
+        //     { localsManifest: res.locals.manifest },
+        //     '\n'.padEnd(220, '='),
+        // );
 
         try {
             return res.render('index', {
