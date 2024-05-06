ecef5dbd4d4755df5039d5b8010458994ac62186
Andy Maskiell <maskiella@gmail.com>
2024-05-05 22:09:45 -0400

    Install tsconfig-paths-webpack-plugin and update lockfile

--- app/.storybook/main.ts
+++ app/.storybook/main.ts
@@ -1,7 +1,9 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
+import TsConfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
+
+// import defaultConfig from '../webpack.config';
 
 const config: StorybookConfig = {
-    stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
     addons: [
         '@storybook/addon-webpack5-compiler-swc',
         '@storybook/addon-onboarding',
@@ -10,12 +12,33 @@ const config: StorybookConfig = {
         '@chromatic-com/storybook',
         '@storybook/addon-interactions',
     ],
+    docs: {
+        autodocs: 'tag',
+    },
     framework: {
         name: '@storybook/react-webpack5',
         options: {},
     },
-    docs: {
-        autodocs: 'tag',
+    stories: ['../src/**/*.mdx', '../src/**/?(*.)stories.@(js|jsx|mjs|ts|tsx)'],
+    webpack: async (config) => {
+        const configRules = config.module?.rules || [];
+        console.log({ configRules, resolve: config.resolve });
+        // const defaultRules = defaultConfig().module.rules;
+        return {
+            ...config,
+            // module: {
+            //     ...config.module,
+            //     // rules: [...configRules, ...defaultRules],
+            // },
+            resolve: {
+                plugins: [
+                    ...(config.resolve?.plugins || []),
+                    new TsConfigPathsPlugin({
+                        extensions: config.resolve?.extensions,
+                    }),
+                ],
+            },
+        };
     },
 };
 export default config;
