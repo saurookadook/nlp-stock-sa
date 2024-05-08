4da425fa2404e398bba32fd5c26d52f7f7a1073c
Andy Maskiell <maskiella@gmail.com>
2024-05-07 20:00:30 -0400

    Clean up home App stories

--- app/src/client/home/components/App/stories.tsx
+++ app/src/client/home/components/App/stories.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 
-// import { App } from 'client/home/components';
 import { getStoryPageData } from 'client/_story-data/articleData';
 import { App } from 'client/home/components';
 import { AppStateProvider } from 'client/home/store';
 
-// const ProviderDecorator = (story) => <AppStateProvider>{story()}</AppStateProvider>;
-
 const meta = {
     title: 'Home/App',
     component: App,
-    // decorators: [ProviderDecorator],
     parameters: {
         // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
         layout: 'fullscreen',
@@ -20,8 +16,6 @@ const meta = {
 
 export default meta;
 type Story = StoryObj<typeof meta>;
-
-// const storyPageData = getStoryPageData();
 
 export const BaseApp: Story = {
     args: {
