b0c6d6d2464d825a3b853a84e7e24aa99a9ab045
Andy Maskiell <maskiella@gmail.com>
2024-05-08 23:35:07 -0400

    Add a funny button to BasePage, small changes to NavHeader

--- app/src/client/common/components/NavHeader/styled.tsx
+++ app/src/client/common/components/NavHeader/styled.tsx
@@ -3,7 +3,7 @@ import styled from '@emotion/styled';
 const NavHeader_nav = styled.nav`
     border-bottom: 1px solid var(--chakra-colors-gray-400);
     /* border-radius: 5px; */
-    padding: 1rem 2rem;
+    padding: 1rem 5rem;
 `;
 
 export { NavHeader_nav };
