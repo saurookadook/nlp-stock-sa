6a5d2b7bca47ec522fbd03fd21689ae4229a320c
Andy Maskiell <maskiella@gmail.com>
2024-05-11 17:21:10 -0400

    Refactor ArticleDataList to include new metadata from articles

--- app/src/client/home/components/ArticleDataList/index.tsx
+++ app/src/client/home/components/ArticleDataList/index.tsx
@@ -8,15 +8,65 @@ import {
     Box,
     Flex,
     Heading,
+    SimpleGrid,
     Spacer,
+    Text,
 } from '@chakra-ui/react';
 
 import type { ArticleDataEntry } from '@nlpssa-app-types/common/main';
+import { BaseLink } from 'client/common/components';
+import { toTitleCase } from 'client/common/utils';
+
+function getMetadataLabelFromKey(metadataKey: string) {
+    const key = metadataKey.replace(/Date$/i, '');
+    return toTitleCase(key);
+}
+
+function ArticleMetadata({ articleRecord }) {
+    const metadataKeys = Object.keys(articleRecord).filter((key) => !/sourceUrl|rawContent|sentenceTokens/im.test(key));
+
+    return (
+        <SimpleGrid
+            columns={2}
+            borderBottom="1px solid"
+            borderColor="gray.400"
+            marginBottom="1rem"
+            paddingBottom="1rem"
+        >
+            {metadataKeys.map((key, i) => {
+                return (
+                    articleRecord[key] && (
+                        <Box key={`${key}-${i}`}>
+                            <b>{getMetadataLabelFromKey(key)}</b>: {articleRecord[key]}
+                        </Box>
+                    )
+                );
+            })}
+            <Box>
+                <b>URL</b>
+                {': '}
+                <BaseLink color="teal" href={articleRecord.sourceUrl} isExternal rel="noopener noreferrer">
+                    {articleRecord.sourceUrl}
+                </BaseLink>
+            </Box>
+        </SimpleGrid>
+    );
+}
 
 function ArticleDataList({ articleData }: React.PropsWithChildren<{ articleData: ArticleDataEntry[] }>) {
+    function getTitleWithFallback(articleRecord) {
+        return (
+            articleRecord.title ||
+            articleRecord.sourceUrl
+                .replace(/^https:\/\/finance.yahoo.com\/(news\/|m\/[^\/]+?[\/])|\.html$/gim, '')
+                .replace(/-/g, ' ')
+                .toUpperCase()
+        );
+    }
+
     return (
         // TODO: maybe this should use `ul` and `li` elements?
-        <Accordion allowMultiple allowToggle>
+        <Accordion allowMultiple>
             {articleData.map((record, i) => (
                 <AccordionItem key={`article-record-${i}`}>
                     <Heading>
@@ -28,22 +78,15 @@ function ArticleDataList({ articleData }: React.PropsWithChildren<{ articleData:
                                 <b>Title</b>
                                 {': '}
                                 {/* TODO: replace with actual title lol */}
-                                {record.sourceUrl
-                                    .replace(/^https:\/\/finance.yahoo.com\/(news\/|m\/[^\/]+?[\/])|\.html$/gim, '')
-                                    .replace(/-/g, ' ')
-                                    .toUpperCase()}
+                                {getTitleWithFallback(record)}
                             </Box>
                             <Spacer />
                             <AccordionIcon />
                         </AccordionButton>
                     </Heading>
                     <AccordionPanel>
-                        <Flex justifyContent="center" textAlign="center" whiteSpace="pre-wrap">
-                            <b>URL</b>
-                            {': '}
-                            <a href={record.sourceUrl} target="_blank" rel="noopener noreferrer">
-                                {record.sourceUrl}
-                            </a>
+                        <Flex flexDirection="column" justifyContent="center">
+                            <ArticleMetadata articleRecord={record} />
                         </Flex>
                         <Flex // force formatting
                             alignItems="flex-start"
@@ -52,20 +95,22 @@ function ArticleDataList({ articleData }: React.PropsWithChildren<{ articleData:
                             width="100%"
                         >
                             <Box // force formatting
+                                className="raw-content"
                                 display="inline-flex"
                                 flexDirection="column"
                                 maxWidth="45%"
                             >
                                 <h2 style={{ fontWeight: 'bold' }}>Raw Content</h2>
-                                <span>{record.rawContent}</span>
+                                <Text>{record.rawContent}</Text>
                             </Box>
                             <Box // force formatting
+                                className="sentence-tokens"
                                 display="inline-flex"
                                 flexDirection="column"
                                 maxWidth="45%"
                             >
                                 <h2 style={{ fontWeight: 'bold' }}>Sentence Tokens</h2>
-                                <span>{record.sentenceTokens}</span>
+                                <Text>{record.sentenceTokens}</Text>
                             </Box>
                         </Flex>
                     </AccordionPanel>
