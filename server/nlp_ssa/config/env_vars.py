33eb2c213408162f5e33bdfab5683213f9395d4a
Andy Maskiell <maskiella@gmail.com>
2024-05-16 14:16:04 -0400

    Fix env_vars and add vars for base urls and auth cookie key

--- server/nlp_ssa/config/env_vars.py
+++ server/nlp_ssa/config/env_vars.py
@@ -1,17 +1,20 @@
 import os
 
-CSRF_SECRET = (os.getenv("CSRF_SECRET", "TMP"),)
-DATABASE_USER = (os.getenv("DATABASE_USER", "postgres"),)
-DATABASE_PASSWORD = (os.getenv("DATABASE_PASSWORD", "example"),)
-DATABASE_HOST = (os.getenv("DATABASE_HOST", "database"),)
-DATABASE_PORT = (os.getenv("DATABASE_PORT", "5432"),)
-DATABASE_NAME = (os.getenv("DATABASE_NAME", "the_money_maker"),)
-ENV = (os.getenv("ENV", "dev"),)
-LOG_LEVEL = (os.getenv("LOG_LEVEL", "DEBUG"),)
-LOG_SQL = (os.getenv("LOG_SQL", False),)
+BASE_API_URL = os.getenv("BASE_API_URL", default="https://nlp-ssa.dev/api")
+BASE_APP_URL = os.getenv("BASE_APP_URL", default="https://nlp-ssa.dev/app")
+
+CSRF_SECRET = os.getenv("CSRF_SECRET", "TMP")
+DATABASE_USER = os.getenv("DATABASE_USER", "postgres")
+DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD", "example")
+DATABASE_HOST = os.getenv("DATABASE_HOST", "database")
+DATABASE_PORT = os.getenv("DATABASE_PORT", "5432")
+DATABASE_NAME = os.getenv("DATABASE_NAME", "the_money_maker")
+ENV = os.getenv("ENV", "dev")
+LOG_LEVEL = os.getenv("LOG_LEVEL", "DEBUG")
+LOG_SQL = os.getenv("LOG_SQL", False)
 
 # Auth
-AUTH_COOKIE_KEY = os.getenv("AUTH_COOKIE_KEY", default="nlp-ssa-auth")
+AUTH_COOKIE_KEY = os.getenv("AUTH_COOKIE_KEY", default="NLPSSA-Authorization.Dev")
 GITHUB_OAUTH_CLIENT_ID = os.getenv("GITHUB_OAUTH_CLIENT_ID", default="")
 GITHUB_OAUTH_CLIENT_SECRET = os.getenv("GITHUB_OAUTH_CLIENT_SECRET", default="")
 GITHUB_OAUTH_CALLBACK_URL = os.getenv("GITHUB_OAUTH_CALLBACK_URL", default="")
