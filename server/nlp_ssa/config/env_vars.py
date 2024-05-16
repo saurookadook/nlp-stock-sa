40e04372b288e76801c293450b369c7a62f794bf
Andy Maskiell <maskiella@gmail.com>
2024-05-16 11:47:53 -0400

    Organize env_vars and add memcached vars

--- server/nlp_ssa/config/env_vars.py
+++ server/nlp_ssa/config/env_vars.py
@@ -1,14 +1,5 @@
 import os
 
-
-GITHUB_OAUTH_CLIENT_ID = os.getenv("GITHUB_OAUTH_CLIENT_ID", default="")
-GITHUB_OAUTH_CLIENT_SECRET = os.getenv("GITHUB_OAUTH_CLIENT_SECRET", default="")
-GITHUB_OAUTH_CALLBACK_URL = os.getenv("GITHUB_OAUTH_CALLBACK_URL", default="")
-GITHUB_OAUTH_AUTH_URL = "https://github.com/login/oauth/authorize"
-GITHUB_OAUTH_TOKEN_URL = "https://github.com/login/oauth/access_token"
-GITHUB_OAUTH_SCOPES = []
-
-
 CSRF_SECRET = (os.getenv("CSRF_SECRET", "TMP"),)
 DATABASE_USER = (os.getenv("DATABASE_USER", "postgres"),)
 DATABASE_PASSWORD = (os.getenv("DATABASE_PASSWORD", "example"),)
@@ -18,3 +9,15 @@ DATABASE_NAME = (os.getenv("DATABASE_NAME", "the_money_maker"),)
 ENV = (os.getenv("ENV", "dev"),)
 LOG_LEVEL = (os.getenv("LOG_LEVEL", "DEBUG"),)
 LOG_SQL = (os.getenv("LOG_SQL", False),)
+
+# Auth
+GITHUB_OAUTH_CLIENT_ID = os.getenv("GITHUB_OAUTH_CLIENT_ID", default="")
+GITHUB_OAUTH_CLIENT_SECRET = os.getenv("GITHUB_OAUTH_CLIENT_SECRET", default="")
+GITHUB_OAUTH_CALLBACK_URL = os.getenv("GITHUB_OAUTH_CALLBACK_URL", default="")
+GITHUB_OAUTH_AUTH_URL = "https://github.com/login/oauth/authorize"
+GITHUB_OAUTH_TOKEN_URL = "https://github.com/login/oauth/access_token"
+GITHUB_OAUTH_SCOPES = []
+
+# Session Cache
+MEMCACHED_HOST = os.getenv("MEMCACHED_HOST", default="session-cache")
+MEMCACHED_PORT = int(os.getenv("MEMCACHED_PORT", default="11211"))
