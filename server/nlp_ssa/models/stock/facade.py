a95c88650259a162bcb67b894b0f1fb4650a59f7
Andy Maskiell <maskiella@gmail.com>
2024-05-18 10:57:02 -0400

    Add and implement StockFacade.create_or_update method

--- server/nlp_ssa/models/stock/facade.py
+++ server/nlp_ssa/models/stock/facade.py
@@ -1,5 +1,8 @@
-from sqlalchemy import select
+import arrow
+from sqlalchemy import literal_column, select
+from sqlalchemy.dialects.postgresql import insert
 from sqlalchemy.orm.exc import NoResultFound
+from typing import Dict
 
 from models.stock import StockDB
 from models.stock.stock import Stock
@@ -22,3 +25,20 @@ class StockFacade:
             raise StockFacade.NoResultFound
 
         return Stock.model_validate(stock)
+
+    def create_or_update(self, *, payload: Dict) -> Stock:
+        insert_stmt = insert(StockDB).values(**payload)
+
+        full_stmt = insert_stmt.on_conflict_do_update(
+            constraint=StockDB.__table__.primary_key,
+            set_={
+                **payload,
+                # "created_at": arrow.utcnow(),
+                "updated_at": arrow.utcnow(),
+            },
+        ).returning(literal_column("*"))
+
+        article_data = self.db_session.execute(full_stmt).fetchone()
+        self.db_session.flush()
+
+        return Stock.model_validate(article_data)
