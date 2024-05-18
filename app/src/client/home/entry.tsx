7e09383446fdc72b37d33c11817ad6659c6ccbff
Andy Maskiell <maskiella@gmail.com>
2024-05-18 11:54:29 -0400

    Fix imports in home and login entry files

--- app/src/client/home/entry.tsx
+++ app/src/client/home/entry.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { ChakraProvider, ColorModeScript, extendTheme } from '@chakra-ui/react';
 
-import { InitialHomePageData } from '@nlpssa-app-types/common/main';
-import { App } from 'client/home/components';
+import type { InitialHomePageData } from '@nlpssa-app-types/common/main';
+import { HomeApp } from 'client/home/components';
 import AppStateProvider from 'client/home/store/AppStateProvider';
 // import reportWebVitals from 'client/reportWebVitals';
 
@@ -20,7 +20,7 @@ window.renderApp = async (initialPageData) => {
         <AppStateProvider>
             <ChakraProvider theme={theme}>
                 <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-                <App initialPageData={initialPageData as InitialHomePageData} />
+                <HomeApp initialPageData={initialPageData as InitialHomePageData} />
             </ChakraProvider>
         </AppStateProvider>,
     );
