675ae51b276956fe0d9774f17ba6873185ce7fb7
Andy Maskiell <maskiella@gmail.com>
2024-05-18 10:56:43 -0400

    Add tests for StockFacade.create_or_update method

--- server/nlp_ssa/models/stock/tests/test_facade.py
+++ server/nlp_ssa/models/stock/tests/test_facade.py
@@ -1,7 +1,11 @@
 import pytest
+from sqlalchemy import select
+from uuid import UUID
 
-from models.stock import Stock, StockFacade
+from models.stock import StockDB, StockFacade
+from models.stock.stock import Stock
 from models.stock.factories import StockFactory
+
 
 @pytest.fixture
 def stock_facade(mock_db_session):
@@ -20,3 +24,59 @@ def test_get_one_by_id(mock_db_session, stock_facade):
 def test_get_one_by_id_no_result(stock_facade):
     with pytest.raises(StockFacade.NoResultFound):
         stock_facade.get_one_by_id(id="415c5a59-942b-4f08-acb5-2c16f2b37c9b")
+
+
+def test_create_or_update_new_stock(mock_db_session, stock_facade):
+    stock_dict = {
+        "id": UUID("5cabc780-62bb-406a-9f63-b97cf7f181b8"),
+        "quote_stock_symbol": "NTDOF",
+        "full_stock_symbol": "Nintendo",
+    }
+
+    result = stock_facade.create_or_update(payload=stock_dict)
+    assert result.id == stock_dict["id"]
+    assert result.quote_stock_symbol == stock_dict["quote_stock_symbol"]
+    assert result.full_stock_symbol == stock_dict["full_stock_symbol"]
+
+    mock_db_session.commit()
+
+    expected = mock_db_session.execute(
+        select(StockDB).where(
+            StockDB.quote_stock_symbol == stock_dict["quote_stock_symbol"]
+        )
+    ).scalar_one()
+
+    assert expected.id == stock_dict["id"]
+    assert expected.quote_stock_symbol == stock_dict["quote_stock_symbol"]
+    assert expected.full_stock_symbol == stock_dict["full_stock_symbol"]
+
+
+def test_create_or_update_existing_stock(mock_db_session, stock_facade):
+    StockFactory(
+        id=UUID("d1bf5370-460b-40d5-90b0-47d711faea8e"),
+        quote_stock_symbol="MEOW",
+        full_stock_symbol="Meow Inc.",
+    )
+    mock_db_session.commit()
+
+    stock_dict = {
+        "quote_stock_symbol": "NTDOF",
+        "full_stock_symbol": "Nintendo",
+    }
+
+    result = stock_facade.create_or_update(payload=stock_dict)
+    assert result.id == stock_dict["id"]
+    assert result.quote_stock_symbol == stock_dict["quote_stock_symbol"]
+    assert result.full_stock_symbol == stock_dict["full_stock_symbol"]
+
+    mock_db_session.commit()
+
+    expected = mock_db_session.execute(
+        select(StockDB).where(
+            StockDB.quote_stock_symbol == stock_dict["quote_stock_symbol"]
+        )
+    ).scalar_one()
+
+    assert expected.id == stock_dict["id"]
+    assert expected.quote_stock_symbol == stock_dict["quote_stock_symbol"]
+    assert expected.full_stock_symbol == stock_dict["full_stock_symbol"]
