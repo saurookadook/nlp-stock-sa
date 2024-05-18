22d1be134c22d60b7fcb43a8fb17f125874b58dd
Andy Maskiell <maskiella@gmail.com>
2024-05-18 12:39:46 -0400

    Add story data for article data by slug page

--- app/src/client/_story-data/index.ts
+++ app/src/client/_story-data/index.ts
@@ -1,5 +1,7 @@
 import * as articleStoryData from './articleData';
+import * as articleStoryDataBySlug from './articleDataBySlug';
 
 export default {
     ...articleStoryData,
+    ...articleStoryDataBySlug,
 };
