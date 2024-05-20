45379aa1184165ad209cc98b3e77993b56e7cb44
Andy Maskiell <maskiella@gmail.com>
2024-05-20 18:24:33 -0400

    Add global ChakraThemeDecorator to storybook preview

--- app/.storybook/preview.tsx
+++ app/.storybook/preview.tsx
@@ -1,6 +1,21 @@
+import React from 'react';
+import { ChakraProvider, ColorModeScript, extendTheme } from '@chakra-ui/react';
 import type { Preview } from '@storybook/react';
 
+const theme = extendTheme({
+    initialColorMode: 'system',
+    useSystemColorMode: true,
+});
+
+const ChakraThemeDecorator = (Story) => (
+    <ChakraProvider>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <Story />
+    </ChakraProvider>
+);
+
 const preview: Preview = {
+    decorators: [ChakraThemeDecorator],
     parameters: {
         controls: {
             matchers: {
