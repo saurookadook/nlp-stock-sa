96c230cda0fb57d438377c624ed2052c6a392f70
Andy Maskiell <maskiella@gmail.com>
2024-05-21 09:29:09 -0400

    Create basic MainNav component

--- app/src/client/common/components/NavHeader/index.tsx
+++ app/src/client/common/components/NavHeader/index.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
-import { Flex, Heading } from '@chakra-ui/react';
+import { Button, Flex, Heading, useDisclosure } from '@chakra-ui/react';
+import { HamburgerIcon } from '@chakra-ui/icons';
 
+import { MainNav } from 'client/common/components';
 import { NavHeader_nav, HomeLink_a } from './styled';
 
 function NavHeader({ ...props }) {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+
     return (
         <NavHeader_nav>
             <Flex alignItems="center" flexDirection="row" {...props}>
+                <Button // force formatting
+                    aria-label="Main navigation button"
+                    colorScheme="teal"
+                    marginRight="1rem"
+                    onClick={onOpen}
+                >
+                    <HamburgerIcon />
+                </Button>
+                <MainNav isOpen={isOpen} onClose={onClose} />
                 <HomeLink_a href="/app">
-                    <Heading>{`💸 🤑 💸 Welcome to NLP SSA 💸 🤑 💸`}</Heading>
+                    <Heading>{`💸 Welcome to NLP SSA 💸`}</Heading>
                 </HomeLink_a>
                 {props.children}
             </Flex>
--- app/src/client/common/components/index.ts
+++ app/src/client/common/components/index.ts
@@ -1,6 +1,7 @@
 import ArticleDataList from 'client/common/components/ArticleDataList';
 import BaseLink from 'client/common/components/BaseLink';
+import MainNav from 'client/common/components/MainNav';
 import NavHeader from 'client/common/components/NavHeader';
 import StockArticleDataGroup from 'client/common/components/StockArticleDataGroup';
 
-export { ArticleDataList, BaseLink, NavHeader, StockArticleDataGroup };
+export { ArticleDataList, BaseLink, MainNav, NavHeader, StockArticleDataGroup };
