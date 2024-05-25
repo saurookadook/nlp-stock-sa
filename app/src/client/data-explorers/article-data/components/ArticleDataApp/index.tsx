4481ace8883e8c51208d9d5874853b92e2d511be
Andy Maskiell <maskiella@gmail.com>
2024-05-25 18:45:30 -0400

    Refactor ArticleDataApp component

--- app/src/client/data-explorers/article-data/components/ArticleDataApp/index.tsx
+++ app/src/client/data-explorers/article-data/components/ArticleDataApp/index.tsx
@@ -1,60 +1,40 @@
 import React, { useContext, useEffect } from 'react';
-import { Center, Container, Flex } from '@chakra-ui/react';
+import { Flex } from '@chakra-ui/react';
 
-import type { InitialArticleDataBySlugPageData } from '@nlpssa-app-types/common/main';
+import { type ArticleDataViewStore } from '@nlpssa-app-types/common/main';
 import { StockArticleDataGroup } from 'client/common/components';
-import { BasePage } from 'client/common/layouts';
 import { BaseStateContext, BaseDispatchContext } from 'client/common/store/contexts';
-import { fetchArticleDataByStockSlug } from 'client/data-explorers/store/actions';
+import { fetchAllArticleData } from 'client/data-explorers/store/actions';
 
-function ArticleDataApp({
-    initialPageData,
-    stockSlug,
-}: {
-    initialPageData: InitialArticleDataBySlugPageData;
-    stockSlug: string;
-}) {
+function ArticleDataView() {
     const state = useContext(BaseStateContext);
     const dispatch = useContext(BaseDispatchContext);
 
-    const pageData = (initialPageData.data || state.pageData) as InitialArticleDataBySlugPageData['data'];
+    const pageData = (state as ArticleDataViewStore).pageData;
 
     useEffect(() => {
         if (state.pageData == null) {
-            fetchArticleDataByStockSlug({ dispatch, stockSlug });
+            fetchAllArticleData({ dispatch });
         }
     });
 
-    console.log('article-data - ArticleDataApp', { initialPageData, state, pageData });
+    console.log('data-explorers.article-data - ArticleDataView', { state, pageData });
     return (
-        <BasePage>
-            <Container className="home" margin="0 auto" maxWidth="75vw">
-                <Center
-                    borderColor="gray.400"
-                    borderRadius="5px"
-                    borderStyle="solid"
-                    borderTopWidth="0px"
-                    borderRightWidth="1px"
-                    borderBottomWidth="0px"
-                    borderLeftWidth="1px"
-                    flexDirection="column"
-                    // h="50vh"
-                    w="100%"
-                >
-                    <Flex className="article-data-list-wrapper" alignSelf="stretch" flexDirection="column">
-                        {pageData != null ? (
-                            <StockArticleDataGroup
-                                quoteStockSymbol={pageData.quoteStockSymbol}
-                                articleData={pageData.articleData}
-                            />
-                        ) : (
-                            'No data :['
-                        )}
-                    </Flex>
-                </Center>
-            </Container>
-        </BasePage>
+        <Flex className="article-data-list-wrapper" alignSelf="stretch" flexDirection="column">
+            {pageData != null && pageData.length > 0
+                ? pageData.map((groupedData, i) => {
+                      const { quoteStockSymbol, articleData } = groupedData;
+                      return (
+                          <StockArticleDataGroup
+                              key={`${quoteStockSymbol}-${i}`}
+                              quoteStockSymbol={quoteStockSymbol}
+                              articleData={articleData}
+                          />
+                      );
+                  })
+                : 'No data :['}
+        </Flex>
     );
 }
 
-export default ArticleDataApp;
+export default ArticleDataView;
