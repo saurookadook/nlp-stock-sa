71589987e968af1ef819deb47b063c54787ba464
Andy Maskiell <maskiella@gmail.com>
2024-05-25 13:44:11 -0400

    Improve all stocks API endpoint

--- server/nlp_ssa/api/routes/api/stocks/router.py
+++ server/nlp_ssa/api/routes/api/stocks/router.py
@@ -1,22 +1,21 @@
-# TODO: make these routes work :]
 import logging
 from fastapi import APIRouter, Depends, Request
 from fastapi_csrf_protect import CsrfProtect
 from sqlalchemy import select
 
-from config import configure_logging
+from api.routes.api.stocks.models import AllStocksResponse
 from db import db_session
 
-# configure_logging(app_name="nlp_ssa.api.routes.stocks")
+
 logger = logging.getLogger(__file__)
 router = APIRouter()
 
 
-@router.get("/api/stocks/all")
+@router.get("/api/stocks/all", response_model=AllStocksResponse)
 async def read_all_stocks():
     from models.stock import StockDB
 
-    all_stocks = db_session.execute(select(StockDB)).scalars().all()
+    all_stocks = db_session.execute(select(StockDB).limit(100)).scalars().all()
 
     return {"data": all_stocks}
 
@@ -25,5 +24,6 @@ async def read_all_stocks():
 async def create_stock_record(
     fast_api_request: Request = None, csrf_protect: CsrfProtect = Depends()
 ):
+    # TODO: make this route work :]
     await csrf_protect.validate_csrf(fast_api_request)
     pass
