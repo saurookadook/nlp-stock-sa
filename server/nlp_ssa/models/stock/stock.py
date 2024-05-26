9730fccaecd496566202fb734df003c426222885
Andy Maskiell <maskiella@gmail.com>
2024-05-26 16:10:32 -0400

    Add exchange_name to Stock Pydantic model

--- server/nlp_ssa/models/stock/stock.py
+++ server/nlp_ssa/models/stock/stock.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel, ConfigDict
 from pydantic.functional_validators import WrapValidator
-from typing import Annotated
+from typing import Annotated, Optional
 from uuid import UUID
 
 from utils.pydantic_helpers import convert_to_arrow_instance
@@ -16,5 +16,6 @@ class Stock(BaseModel):
     full_stock_symbol: str = (
         ""  # TODO: for some bizarre reason, these raise errors without the default value
     )
+    exchange_name: Optional[str]
     created_at: Annotated[str, WrapValidator(convert_to_arrow_instance)]
     updated_at: Annotated[str, WrapValidator(convert_to_arrow_instance)]
