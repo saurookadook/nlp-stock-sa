1121bee41b86f7328eb92b9f897bda9d94f3cf89
Andy Maskiell <maskiella@gmail.com>
2024-05-26 19:50:29 -0400

    Refactor and clean up BaseDataExplorer layout component

--- app/src/client/data-explorers/layouts/BaseDataExplorer/index.tsx
+++ app/src/client/data-explorers/layouts/BaseDataExplorer/index.tsx
@@ -1,44 +1,12 @@
 import React from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 
 import type { AbstractPageData } from '@nlpssa-app-types/common/main';
-import { BasePage, ErrorElement } from 'client/common/layouts';
-import {
-    ArticleDataBySlugExplorer,
-    ArticleDataExplorer,
-    AllStocksExplorer,
-    SingleStockExplorer,
-} from 'client/data-explorers/explorers';
-import { DataExplorerView } from 'client/data-explorers/layouts';
+import { BasePage } from 'client/common/layouts';
 import AppStateProvider from 'client/data-explorers/store/AppStateProvider';
+import browserRouter from 'client/data-explorers/browserRouter';
 
 function BaseDataExplorer({ initialPageData }: { initialPageData: AbstractPageData }) {
-    const browserRouter = createBrowserRouter([
-        {
-            path: '/app/data-explorers',
-            element: <DataExplorerView />,
-            errorElement: <ErrorElement />,
-            children: [
-                {
-                    path: 'article-data/:stockSlug',
-                    element: <ArticleDataBySlugExplorer />,
-                },
-                {
-                    path: 'article-data',
-                    element: <ArticleDataExplorer />,
-                },
-                {
-                    path: 'stocks/:stockSlug',
-                    element: <SingleStockExplorer />,
-                },
-                {
-                    path: 'stocks',
-                    element: <AllStocksExplorer />,
-                },
-            ],
-        },
-    ]);
-
     console.log('data-explorers - BaseDataExplorer', { initialPageData });
     return (
         <BasePage>
