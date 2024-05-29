37d6bfa12d57755258317e80e459ba68388a6097
Andy Maskiell <maskiella@gmail.com>
2024-05-28 22:43:06 -0400

    Fix colors for heading in StockArticleDataGroup

--- app/src/client/common/components/StockArticleDataGroup/index.tsx
+++ app/src/client/common/components/StockArticleDataGroup/index.tsx
@@ -6,15 +6,26 @@ import {
     Link,
     Spacer,
     Text,
+    useColorModeValue,
 } from '@chakra-ui/react';
 import { ArticleDataList } from 'client/common/components';
 
 function StockArticleDataGroup({ articleData, quoteStockSymbol, routerDomAware = false }) {
+    const headingBackgroundColor = useColorModeValue('teal.500', 'teal.200');
+    const headingColor = useColorModeValue('white', 'gray.800');
+
     const seeAllTarget = `/app/data-explorers/article-data/${quoteStockSymbol}`;
 
     return (
         <Box>
-            <Heading alignItems="center" backgroundColor="teal" color="white" display="flex" padding="0.5rem 1rem">
+            <Heading
+                alignItems="center" // force formatting
+                backgroundColor={headingBackgroundColor}
+                color={headingColor}
+                colorScheme="teal"
+                display="flex"
+                padding="0.5rem 1rem"
+            >
                 <Box as="span" fontWeight="700">
                     {quoteStockSymbol}
                 </Box>
