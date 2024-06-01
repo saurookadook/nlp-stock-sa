07a6ee0b8d973200e944fbcf3ec0433cd632b757
Andy Maskiell <maskiella@gmail.com>
2024-06-01 14:00:52 -0400

    Implement remaining test cases

--- server/nlp_ssa/models/sentiment_analysis/tests/test_facade.py
+++ server/nlp_ssa/models/sentiment_analysis/tests/test_facade.py
@@ -1,10 +1,14 @@
+import arrow
 import pytest
-from uuid import UUID
+from sqlalchemy import select
+from uuid import UUID, uuid4
 
 from models.sentiment_analysis import (
     SentimentAnalysis,
+    SentimentAnalysisDB,
     SentimentAnalysisFacade,
 )
+from models.sentiment_analysis.constants import SentimentEnum
 from models.sentiment_analysis.factories import SentimentAnalysisFactory
 from models.stock.factories import StockFactory
 from utils.testing_mocks import get_mock_utcnow
@@ -55,7 +59,27 @@ def test_get_one_by_source_group_id_no_result(sentiment_analysis_facade):
 
 
 def test_get_all_by_stock_symbol(mock_db_session, sentiment_analysis_facade):
-    assert "Implement me! :D" is False
+    mock_stock_1 = StockFactory(quote_stock_symbol="FOO")
+    mock_stock_2 = StockFactory(quote_stock_symbol="BAR")
+    mock_db_session.commit()
+
+    mock_sentiment_analysis_1 = SentimentAnalysisFactory(
+        quote_stock_symbol=mock_stock_1.quote_stock_symbol
+    )
+    SentimentAnalysisFactory(quote_stock_symbol=mock_stock_2.quote_stock_symbol)
+    mock_sentiment_analysis_3 = SentimentAnalysisFactory(
+        quote_stock_symbol=mock_stock_1.quote_stock_symbol
+    )
+    mock_db_session.commit()
+
+    results = sentiment_analysis_facade.get_all_by_stock_symbol(
+        mock_stock_1.quote_stock_symbol
+    )
+
+    assert results == [
+        SentimentAnalysis.model_validate(mock_sentiment_analysis_1),
+        SentimentAnalysis.model_validate(mock_sentiment_analysis_3),
+    ]
 
 
 def test_get_all_by_stock_symbol_no_results(sentiment_analysis_facade):
@@ -65,10 +89,65 @@ def test_get_all_by_stock_symbol_no_results(sentiment_analysis_facade):
 
 
 def test_create_or_update_new_article_data(mock_db_session, sentiment_analysis_facade):
-    assert "Implement me! :D" is False
+    StockFactory(quote_stock_symbol="NTDOF")
+    mock_db_session.commit()
+
+    sentiment_analysis_dict = {
+        "id": uuid4(),
+        "quote_stock_symbol": "NTDOF",
+        "source_group_id": UUID("3a0e5f09-3904-46df-bffb-13f5a95412ad"),
+        "score": 0.9,
+        "sentiment": SentimentEnum.POSITIVE,
+    }
+
+    result = sentiment_analysis_facade.create_or_update(payload=sentiment_analysis_dict)
+
+    assert result.quote_stock_symbol == sentiment_analysis_dict["quote_stock_symbol"]
+    assert result.source_group_id == sentiment_analysis_dict["source_group_id"]
+    assert result.score == sentiment_analysis_dict["score"]
+    assert result.sentiment == sentiment_analysis_dict["sentiment"]
+    # TODO: find better way to mock server 'now' function
+    assert isinstance(result.created_at, arrow.Arrow)
+    assert isinstance(result.updated_at, arrow.Arrow)
 
 
 def test_create_or_update_existing_article_data(
     mock_db_session, sentiment_analysis_facade
 ):
-    assert "Implement me! :D" is False
+    mock_stock = StockFactory(quote_stock_symbol="DIS")
+    mock_db_session.commit()
+
+    mock_sentiment_analysis = SentimentAnalysisFactory(
+        id=UUID("7fb2c6c5-b2e3-4bd0-9b84-22fbeb729d8c"),
+        quote_stock_symbol=mock_stock.quote_stock_symbol,
+        score=0.5,
+    )
+    mock_db_session.commit()
+
+    updated_sentiment_analysis_dict = {
+        "id": mock_sentiment_analysis.id,
+        "quote_stock_symbol": mock_sentiment_analysis.quote_stock_symbol,
+        "source_group_id": mock_sentiment_analysis.source_group_id,
+        "score": 0.9,
+        "sentiment": SentimentEnum.POSITIVE,
+    }
+
+    sentiment_analysis_facade.create_or_update(payload=updated_sentiment_analysis_dict)
+    mock_db_session.commit()
+
+    sentiment_analysis_db = mock_db_session.execute(
+        select(SentimentAnalysisDB).where(
+            SentimentAnalysisDB.id == updated_sentiment_analysis_dict["id"]
+        )
+    ).scalar_one()
+
+    result = SentimentAnalysis.model_validate(sentiment_analysis_db)
+
+    assert result.id == mock_sentiment_analysis.id
+    assert result.quote_stock_symbol == mock_sentiment_analysis.quote_stock_symbol
+    assert result.source_group_id == mock_sentiment_analysis.source_group_id
+    assert result.score == updated_sentiment_analysis_dict["score"]
+    assert result.sentiment == updated_sentiment_analysis_dict["sentiment"]
+    assert result.created_at == get_mock_utcnow()
+    # TODO: find better way to mock server 'now' function
+    assert isinstance(result.updated_at, arrow.Arrow)
