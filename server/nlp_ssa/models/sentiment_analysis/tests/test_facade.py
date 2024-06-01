2af600df2b6e999b9101f1ae0ccd61bd3c05fccf
Andy Maskiell <maskiella@gmail.com>
2024-06-01 13:40:41 -0400

    Stub out SentimentAnalysisFacade method tests

--- server/nlp_ssa/models/sentiment_analysis/tests/test_facade.py
+++ server/nlp_ssa/models/sentiment_analysis/tests/test_facade.py
@@ -6,6 +6,7 @@ from models.sentiment_analysis import (
 )
 from models.sentiment_analysis.factories import SentimentAnalysisFactory
 from models.stock.factories import StockFactory
+from utils.testing_mocks import get_mock_utcnow
 
 
 def test_get_one_by_id(mock_db_session, sentiment_analysis_facade):
@@ -27,3 +28,31 @@ def test_get_one_by_id_no_result(sentiment_analysis_facade):
         sentiment_analysis_facade.get_one_by_id(
             id="ba379b11-6c65-4cf9-af09-43c5ae41e979"
         )
+
+
+def test_get_one_by_source_group_id(mock_db_session, sentiment_analysis_facade):
+    assert "Implement me! :D" is False
+
+
+def test_get_one_by_source_group_id_no_result(
+    mock_db_session, sentiment_analysis_facade
+):
+    assert "Implement me! :D" is False
+
+
+def test_get_all_by_stock_symbol(mock_db_session, sentiment_analysis_facade):
+    assert "Implement me! :D" is False
+
+
+def test_get_all_by_stock_symbol_no_results(mock_db_session, sentiment_analysis_facade):
+    assert "Implement me! :D" is False
+
+
+def test_create_or_update_new_article_data(mock_db_session, sentiment_analysis_facade):
+    assert "Implement me! :D" is False
+
+
+def test_create_or_update_existing_article_data(
+    mock_db_session, sentiment_analysis_facade
+):
+    assert "Implement me! :D" is False
