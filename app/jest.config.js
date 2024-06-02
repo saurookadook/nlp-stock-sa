c67ce93c88fca88fe06b1399662445d49c6e3e92
Andy Maskiell <maskiella@gmail.com>
2024-06-02 16:11:09 -0400

    Add ts-jest and generate basic jest config

---
+++ app/jest.config.js
@@ -0,0 +1,7 @@
+/**
+ * Base config created from {@link https://kulshekhar.github.io/ts-jest/docs/getting-started/installation#jest-config-file|`yarn ts-jest config:init`}
+ */
+export default {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+};
