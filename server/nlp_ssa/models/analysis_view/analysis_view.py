f00fcac647104d2add431213c856a1628019830d
Andy Maskiell <maskiella@gmail.com>
2024-06-06 10:05:38 -0400

    Refactor Pydantic models to use TimestampsMixin

--- server/nlp_ssa/models/analysis_view/analysis_view.py
+++ server/nlp_ssa/models/analysis_view/analysis_view.py
@@ -1,17 +1,13 @@
 from pydantic import BaseModel, ConfigDict
-from pydantic.functional_validators import WrapValidator
-from typing import Annotated, Optional
 from uuid import UUID
 
-from utils.pydantic_helpers import convert_to_arrow_instance
+from models.mixins import TimestampsMixin
 
 
-class AnalysisView(BaseModel):
+class AnalysisView(BaseModel, TimestampsMixin):
     model_config = ConfigDict(from_attributes=True)
 
     id: UUID
     source_group_id: UUID
     # owner_id: UUID
     user_id: UUID
-    created_at: Annotated[str, WrapValidator(convert_to_arrow_instance)]
-    updated_at: Annotated[str, WrapValidator(convert_to_arrow_instance)]
--- server/nlp_ssa/models/sentiment_analysis/sentiment_analysis.py
+++ server/nlp_ssa/models/sentiment_analysis/sentiment_analysis.py
@@ -7,14 +7,23 @@ from models.sentiment_analysis import SentimentEnum
 from utils.pydantic_helpers import convert_to_arrow_instance
 
 
+class AnalysisOutput(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    compound: float
+    neg: float
+    neu: float
+    pos: float
+
+
 class SentimentAnalysis(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
     id: UUID
     quote_stock_symbol: str
+    source_group_id: UUID
     score: float
     sentiment: SentimentEnum
-    # TODO: add 'output' column
-    source_group_id: UUID
+    output: AnalysisOutput
     created_at: Annotated[str, WrapValidator(convert_to_arrow_instance)]
     updated_at: Annotated[str, WrapValidator(convert_to_arrow_instance)]
