6254169ae33f328c8faac5d2decc95db79e7fb19
Andy Maskiell <maskiella@gmail.com>
2024-06-08 17:02:39 -0400

    Refactor enum classes in constants

--- server/nlp_ssa/constants/__init__.py
+++ server/nlp_ssa/constants/__init__.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from typing import Literal
 
 
 class SentimentEnum(Enum):
@@ -8,11 +9,19 @@ class SentimentEnum(Enum):
     NEUTRAL = "neutral"
     POSITIVE = "positive"
 
+    @staticmethod
+    def db_type_name() -> Literal["sentiments"]:
+        return "sentiments"
+
 
 class SourceDiscriminatorEnum(Enum):
 
     ARTICLE_DATA = "ArticleDataDB"
     REDDIT_DATA = "RedditDataDB"
+
+    @staticmethod
+    def db_type_name() -> Literal["source_discriminators"]:
+        return "source_discriminators"
 
 
 ONE_DAY_IN_SECONDS = 60 * 60 * 24
