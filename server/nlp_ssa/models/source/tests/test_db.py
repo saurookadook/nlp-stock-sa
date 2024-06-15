801d35cec197ce77e0dfb5f24a14753747c2650c
Andy Maskiell <maskiella@gmail.com>
2024-06-15 14:15:44 -0400

    Stub out SourceDB tests

--- server/nlp_ssa/models/source/tests/test_db.py
+++ server/nlp_ssa/models/source/tests/test_db.py
@@ -4,15 +4,20 @@ from sqlalchemy import select, and_
 from uuid import UUID
 
 from models.article_data.factories import ArticleDataFactory
-from models.source import SourceDB
-
-# from models.source.factories import SourceFactory
 from models.sentiment_analysis.factories import SentimentAnalysisFactory
+from models.source import SourceDB
+from models.source.factories import SourceFactory
+from models.stock.factories import StockFactory
 
 
 @pytest.fixture
-def mock_article_data():
-    return ArticleDataFactory()
+def mock_stock(mock_db_session):
+    ntdof_stock = StockFactory(quote_stock_symbol="NTDOF")
+
+
+@pytest.fixture
+def mock_sentiment_analysis(mock_db_session):
+    sentiment_analysis_mock = SentimentAnalysisFactory()
 
 
 def test_source_db(mock_db_session):
