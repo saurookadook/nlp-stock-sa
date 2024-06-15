fedbc3b27aba72e86794bfbfbf7117b2d576c4ca
Andy Maskiell <maskiella@gmail.com>
2024-06-15 14:29:57 -0400

    Small refactor to shared Enums

--- server/nlp_ssa/constants/__init__.py
+++ server/nlp_ssa/constants/__init__.py
@@ -2,7 +2,22 @@ from enum import Enum
 from typing import Literal
 
 
-class SentimentEnum(Enum):
+class ExtendedEnum(Enum):
+    @classmethod
+    def get_default_value(cls):
+        """Inheriting classes must implement a `get_default` method"""
+        return cls.get_default().value
+
+    @classmethod
+    def get_by_value(cls, value):
+        for member in cls:
+            if member.value == value:
+                return member
+
+        return None
+
+
+class SentimentEnum(ExtendedEnum):
 
     COMPOUND = "compound"
     NEGATIVE = "negative"
@@ -13,8 +28,12 @@ class SentimentEnum(Enum):
     def db_type_name() -> Literal["sentiments"]:
         return "sentiments"
 
+    @classmethod
+    def get_default(cls):
+        return cls.NEUTRAL
 
-class SourceDiscriminatorEnum(Enum):
+
+class SourceDiscriminatorEnum(ExtendedEnum):
 
     # ARTICLE_DATA = "ArticleDataDB"
     # REDDIT_DATA = "RedditDataDB"
@@ -28,18 +47,6 @@ class SourceDiscriminatorEnum(Enum):
     @classmethod
     def get_default(cls):
         return cls.ArticleDataDB
-
-    @classmethod
-    def get_default_value(cls):
-        return cls.get_default().value
-
-    @classmethod
-    def get_by_value(cls, value):
-        for member in cls:
-            if member.value == value:
-                return member
-
-        return None
 
 
 ONE_DAY_IN_SECONDS = 60 * 60 * 24
