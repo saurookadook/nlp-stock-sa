cbeedda38f0889f8eb92f8cc55712697f426514d
Andy Maskiell <maskiella@gmail.com>
2024-06-17 20:59:11 -0400

    Add OwnedByPolymorphicSourceMixin for Pydantic models

--- server/nlp_ssa/models/mixins/__init__.py
+++ server/nlp_ssa/models/mixins/__init__.py
@@ -1,3 +1,2 @@
-from models.mixins.db import TimestampsDB
-from models.mixins.factories import TimestampsMixinFactory
-from models.mixins.pydantic import TimestampsMixin
+from models.mixins.db import TimestampsDB, OwnedByPolymorphicSourceDB
+from models.mixins.pydantic import TimestampsMixin, OwnedByPolymorphicSourceMixin
--- server/nlp_ssa/models/mixins/pydantic.py
+++ server/nlp_ssa/models/mixins/pydantic.py
@@ -1,4 +1,17 @@
-from utils.pydantic_helpers import SerializerArrowType
+from pydantic import Field, field_validator
+from typing import Annotated, Optional
+
+from models.source import Source
+from utils.pydantic_helpers import SerializerArrowType, generic_validator_with_default
+
+
+class OwnedByPolymorphicSourceMixin:
+    polymorphic_source: Annotated[Optional[Source], Field(default_factory=lambda: None)]
+
+    @field_validator("polymorphic_source")
+    @classmethod
+    def handle_field_default(cls, value, info):
+        return generic_validator_with_default(cls, value, info)
 
 
 class TimestampsMixin:
