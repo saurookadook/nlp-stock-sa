838dd35a918975a7da51a7d1fdc6c77e9a3998a1
Andy Maskiell <maskiella@gmail.com>
2024-06-19 17:52:08 -0400

    Add sentimentAnalyses slices to data-explorers store

--- app/src/client/data-explorers/store/reducer.ts
+++ app/src/client/data-explorers/store/reducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from 'client/common/store/utils';
 import * as articleDataSlices from 'client/data-explorers/store/article-data/reducer';
+import * as sentimentAnalysesSlices from 'client/data-explorers/store/sentiment-analyses/reducer';
 import * as stocksSlices from 'client/data-explorers/store/stocks/reducer';
 
 export default combineReducers({
     ...articleDataSlices,
+    ...sentimentAnalysesSlices,
     ...stocksSlices,
 });
