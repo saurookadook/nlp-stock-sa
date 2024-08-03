e8640aaab46ee52905807b1be347097d7b6e64a2
Andy Maskiell <maskiella@gmail.com>
2024-08-03 11:16:01 -0400

    Create constants for MultiSeriesLineGraph

---
+++ app/src/client/common/components/MultiSeriesLineGraph/constants.ts
@@ -0,0 +1,8 @@
+export const polarities = ['Compound', 'Negative', 'Neutral', 'Positive'];
+
+export const strokeColorByPolarity = {
+    Compound: 'steelblue',
+    Negative: 'red',
+    Neutral: 'orange',
+    Positive: 'green',
+};
