af1c0c260d12f3657878ae4f6cc857a99c098199
Andy Maskiell <maskiella@gmail.com>
2024-08-03 11:19:14 -0400

    Add Legend to MultiSeriesLineGraph

--- app/src/client/common/components/MultiSeriesLineGraph/index.tsx
+++ app/src/client/common/components/MultiSeriesLineGraph/index.tsx
@@ -1,8 +1,14 @@
 import React, { useEffect, useRef } from 'react';
 import * as d3 from 'd3';
 import styled from '@emotion/styled';
+import { Flex } from '@chakra-ui/react';
 
 import type { SentimentAnalysesDataEntry } from '@nlpssa-app-types/common/main';
+import Legend from './Legend';
+
+const StyledGraphWrapper = styled(Flex)`
+    flex-direction: row;
+`;
 
 const StyledSVG = styled.svg`
     font: 10px sans-serif;
@@ -16,13 +22,12 @@ const polarities = ['Compound', 'Negative', 'Neutral', 'Positive'];
 const strokeColorByPolarity = {
     Compound: 'steelblue',
     Negative: 'red',
-    Neutral: 'black',
+    Neutral: 'orange',
     Positive: 'green',
 };
 
-// type DispatchParams = d3.CustomEventParameters & { bubble?: boolean };
+type DispatchParams = d3.CustomEventParameters & { bubble?: boolean };
 
-// type D3Point = [number, number, string];
 type D3Point = [Date, number, string];
 
 type PointsByPolarityType = {
@@ -32,38 +37,17 @@ type PointsByPolarityType = {
     Positive: D3Point[];
 };
 
-/**
- * @description TODO: this thing is doing way too much stuff at the moment; \
- * eventually, it should _only_ be sorting by last_update_date or published_date :)
- * @param a
- * @param b
- */
-function compareByDateCallback(a: SentimentAnalysesDataEntry, b: SentimentAnalysesDataEntry) {
-    const aField = a.source!.data!.last_updated_date as Date;
-    const bField = b.source!.data!.last_updated_date as Date;
-    // let aField: Date = new Date(a.source!.data!.created_at);
-    // let bField: Date = new Date(b.source!.data!.created_at);
-
-    // if (dateFieldIsValid(a.source?.data?.last_updated_date) && dateFieldIsValid(b.source?.data?.last_updated_date)) {
-    //     aField = a.source!.data!.last_updated_date as Date;
-    //     bField = b.source!.data!.last_updated_date as Date;
-    // } else if (dateFieldIsValid(a.source?.data?.published_date) && dateFieldIsValid(b.source?.data?.published_date)) {
-    //     aField = a.source!.data!.published_date as Date;
-    //     bField = b.source!.data!.published_date as Date;
-    // }
-
-    return aField == bField ? 0 : Number(aField > bField) - Number(aField < bField);
-}
-
-function dateFieldIsValid(dateField: unknown) {
-    return dateField != null && typeof dateField === 'string' && dateField !== '';
-}
+type LinesPathsRef = d3.Selection<SVGPathElement | d3.BaseType, D3Point[] & { z: string }, SVGGElement, unknown> & {
+    _groups: [SVGPathElement, number][];
+};
 
 // 640 / 16 = 40
 // 400 / 16 = 25
 // -- 40 / 25 = 1.6
 function MultiSeriesLineGraph({
-    data,
+    sentimentAnalysesData,
+    legendItemSize = 20,
+    legendSpacer = 16,
     width = 768,
     height = 480,
     marginTop = 20,
@@ -71,7 +55,9 @@ function MultiSeriesLineGraph({
     marginBottom = 30,
     marginLeft = 40,
 }: React.PropsWithChildren<{
-    data: SentimentAnalysesDataEntry[];
+    sentimentAnalysesData: SentimentAnalysesDataEntry[];
+    legendItemSize?: number;
+    legendSpacer?: number;
     width?: number;
     height?: number;
     marginTop?: number;
@@ -85,27 +71,12 @@ function MultiSeriesLineGraph({
     // const linesPaths = useRef<SVGGElement>(null);
     const dot = useRef<SVGGElement>(null);
 
-    // TODO: the creation of the date fields here is only temporary; that kind of
-    // normalization should probably be done in the reducer
-    const sortedDataWithSources = data
-        .filter((d) => d.source != null)
-        .map(function (d) {
-            if (dateFieldIsValid(d.source?.data?.last_updated_date)) {
-                d.source!.data!.last_updated_date = new Date(d.source!.data!.last_updated_date as string);
-            }
-            if (dateFieldIsValid(d.source?.data?.published_date)) {
-                d.source!.data!.published_date = new Date(d.source!.data!.published_date as string);
-            }
-            return d;
-        })
-        .sort(compareByDateCallback);
-
     const dateValue = (d: SentimentAnalysesDataEntry) =>
         (d.source?.data?.last_updated_date || d.source?.data?.published_date) as Date;
 
     const x = d3
         .scaleUtc()
-        .domain(d3.extent(sortedDataWithSources, dateValue) as [Date, Date])
+        .domain(d3.extent(sentimentAnalysesData, dateValue) as [Date, Date])
         .range([marginLeft, width - marginRight]);
 
     const y = d3
@@ -114,7 +85,26 @@ function MultiSeriesLineGraph({
         .nice()
         .range([height - marginBottom, marginTop]);
 
-    const compoundPoints = sortedDataWithSources.reduce(
+    const points = sentimentAnalysesData.reduce(function (acc: D3Point[], d: SentimentAnalysesDataEntry) {
+        acc.push([
+            d.source!.data!.last_updated_date as Date,
+            d.output.compound,
+            `${d.quoteStockSymbol}: Compound score`,
+        ]);
+        acc.push([d.source!.data!.last_updated_date as Date, d.output.neg, `${d.quoteStockSymbol}: Negative score`]);
+        acc.push([d.source!.data!.last_updated_date as Date, d.output.neu, `${d.quoteStockSymbol}: Neutral score`]);
+        acc.push([d.source!.data!.last_updated_date as Date, d.output.pos, `${d.quoteStockSymbol}: Positive score`]);
+
+        return acc;
+    }, []);
+
+    const groups = d3.rollup(
+        points,
+        (v) => Object.assign(v, { z: v[0][2] }),
+        (d) => d[2],
+    );
+
+    const compoundPoints = sentimentAnalysesData.reduce(
         function (acc: PointsByPolarityType, d: SentimentAnalysesDataEntry) {
             // could probably remove this at some point...
             if (d.source != null) {
@@ -158,35 +148,98 @@ function MultiSeriesLineGraph({
         .x((d) => x(d[0]))
         .y((d) => y(d[1]));
 
+    let linesPaths;
+
+    useEffect(() => {
+        if (svgRef.current) {
+            linesPaths = d3
+                .select(svgRef.current)
+                .append('g')
+                .classed('lines-paths', true)
+                .attr('fill', 'none')
+                .attr('stroke-width', 1.5)
+                .attr('stroke-linejoin', 'round')
+                .attr('stroke-linecap', 'round')
+                .selectAll('path')
+                .data(groups.values())
+                .join('path');
+
+            linesPaths._groups[0].map(function (path: SVGPathElement, i: number) {
+                const polarity = polarities[i];
+                return d3
+                    .select(path)
+                    .classed('line-path', true)
+                    .attr('aria-label', polarity)
+                    .style('stroke', strokeColorByPolarity[polarity])
+                    .attr('d', lineGenerator as (data) => string);
+                // .attr('d', lineGenerator(compoundPoints[polarity]) as string)
+            });
+        }
+    }, []);
+
     // When the pointer moves, find the closest point, update the interactive tip, and highlight
     // the corresponding line. Note: we don't actually use Voronoi here, since an exhaustive search
     // is fast enough.
-    // function pointermoved(event) {
-    //     const [xm, ym] = d3.pointer(event);
-    //     const i = Number(d3.leastIndex(compoundPoints, ([x, y]) => Math.hypot(Number(x) - xm, Number(y) - ym)));
-    //     const [x, y, k] = compoundPoints[i];
-    //     // TODO: fix weird type error
-    //     // d3.select(linesPaths.current).style("stroke", ({ z }) => z === k ? null : "#ddd").filter(({ z }) => z === k).raise();
-    //     const dotEl = d3.select(dot.current);
-    //     dotEl.attr('transform', `translate(${x},${y})`);
-    //     dotEl.select('text').text(k);
-    //     d3.select(svgRef.current)
-    //         .property('value', data[i])
-    //         .dispatch('input', { bubbles: true } as DispatchParams);
-    // }
+    function pointermoved(event) {
+        // console.groupCollapsed('pointermoved callback - event');
+        console.log(event, points);
+        // console.groupEnd();
+        try {
+            const [eventX, eventY] = d3.pointer(event);
+            const i = Number(d3.leastIndex(points, ([x, y]) => Math.hypot(Number(x) - eventX, Number(y) - eventY)));
+            const [x, y, k] = points[i];
+            // TODO: fix weird type error
+            // console.groupCollapsed('pointermoved callback - other vars');
+            console.log({ eventX, eventY, i, x, y, k });
+            // console.groupEnd();
+            const paths = d3.select(svgRef.current).selectAll('path.line-path');
+            // console.log({ paths });
+            paths.each(function (d, i, nodes) {
+                const el = nodes[i] as SVGPathElement;
+                const polarityLabel = el.getAttribute('aria-label') || '';
+                // console.log({ el, polarityForNode: polarities[i], polarityLabel });
+                if (k.indexOf(polarityLabel) > -1) {
+                    d3.select(el).raise();
+                } else {
+                    el.style.stroke = '#dddddd';
+                }
+            });
+            d3.select(dot.current) // force formatting
+                .attr('transform', `translate(${x},${y})`)
+                .select('text')
+                .text(k);
+            d3.select(svgRef.current)
+                .property('value', sentimentAnalysesData[i])
+                .dispatch('input', { bubbles: true } as DispatchParams);
+        } catch (e) {
+            // console.groupCollapsed('ERROR: pointermoved');
+            console.error(e);
+            // console.groupEnd();
+        }
+    }
 
-    // function pointerentered() {
-    //     d3.select(linesPaths.current).style('mix-blend-mode', null).style('stroke', '#ddd');
-    //     d3.select(dot.current).attr('display', null);
-    // }
+    function pointerentered(event) {
+        console.groupCollapsed('pointerentered callback');
+        console.log({ event });
+        console.groupEnd();
+        d3.select(linesPaths.current).style('mix-blend-mode', null).style('stroke', '#ddd');
+        d3.select(dot.current).attr('display', null);
+    }
 
-    // function pointerleft() {
-    //     d3.select(linesPaths.current).style('mix-blend-mode', 'multiply').style('stroke', null);
-    //     d3.select(dot.current).attr('display', 'none');
-    //     // TODO: fix weird type error
-    //     // d3.select(svgRef.current).node().value = null;
-    //     d3.select(svgRef.current).dispatch('input', { bubbles: true } as DispatchParams);
-    // }
+    function pointerleft() {
+        const paths = d3.select(svgRef.current).selectAll('path.line-path');
+        // console.log({ paths });
+        paths.each(function (d, i, nodes) {
+            const el = nodes[i] as SVGPathElement;
+            const polarityLabel = el.getAttribute('aria-label') || '';
+            // console.log({ el, polarityForNode: polarities[i], polarityLabel });
+            el.style.stroke = strokeColorByPolarity[polarityLabel];
+        });
+        d3.select(dot.current).attr('display', 'none');
+        // TODO: fix weird type error
+        // d3.select(svgRef.current).node().value = null;
+        d3.select(svgRef.current).dispatch('input', { bubbles: true } as DispatchParams);
+    }
 
     useEffect(() => {
         if (gx.current != null) {
@@ -224,37 +277,42 @@ function MultiSeriesLineGraph({
     }, []);
 
     return (
-        <StyledSVG
-            ref={svgRef}
-            width={width}
-            height={height}
-            viewBox={`0 0 ${width} ${height}`}
-            // onPointerEnter={pointerentered}
-            // onPointerMove={pointermoved}
-            // onPointerLeave={pointerleft}
-            onTouchStart={(event) => event.preventDefault()}
-        >
-            <g ref={gx} transform={`translate(0,${height - marginBottom})`} />
-            <g ref={gy} transform={`translate(${marginLeft},0)`} />
-            {polarities.map((pol, i) => (
-                <path
-                    key={`polarity-score-line-${i}`}
-                    fill="none"
-                    stroke={strokeColorByPolarity[pol]}
-                    strokeWidth="1.5"
-                    d={lineGenerator(compoundPoints[pol]) as string}
-                />
-            ))}
-            <g ref={dot} display="none" fill="white" stroke="currentColor" strokeWidth="1.5">
-                {data.map((d, i) => (
-                    // cx={x(i)} cy={y(d)}
-                    <circle key={`circle-${i}`} r="2.5" />
-                ))}
-                {data.map((d, i) => (
-                    <text key={i} textAnchor="middle" y={y(d.output.compound)} r="2.5" />
-                ))}
-            </g>
-        </StyledSVG>
+        <StyledGraphWrapper>
+            <StyledSVG
+                ref={svgRef}
+                width={width}
+                height={height + legendItemSize + legendSpacer}
+                viewBox={`0 0 ${width} ${height}`}
+                onPointerEnter={pointerentered}
+                onPointerMove={pointermoved}
+                onPointerLeave={pointerleft}
+                onTouchStart={(event) => event.preventDefault()}
+            >
+                <g ref={gx} transform={`translate(0,${height - marginBottom})`} />
+                <g ref={gy} transform={`translate(${marginLeft},0)`} />
+                {/* <g ref={linesPathsRef} className="lines-paths" /> */}
+                {/* {polarities.map((pol, i) => (
+                    <path
+                        key={`polarity-score-line-${i}`}
+                        className="line-path"
+                        fill="none"
+                        stroke={strokeColorByPolarity[pol]}
+                        strokeWidth="1.5"
+                        d={lineGenerator(compoundPoints[pol]) as string}
+                    />
+                ))} */}
+                <g ref={dot} display="none" fill="white" stroke="currentColor" strokeWidth="1.5">
+                    {sentimentAnalysesData.map((d, i) => (
+                        // cx={x(i)} cy={y(d)}
+                        <>
+                            <circle key={`circle-${i}`} r="2.5" />
+                            <text key={`sa-text-${i}`} textAnchor="middle" y={-8} r="2.5" />
+                        </>
+                    ))}
+                </g>
+                <Legend height={height} legendItemSize={legendItemSize} legendSpacer={legendSpacer} />
+            </StyledSVG>
+        </StyledGraphWrapper>
     );
 }
 
