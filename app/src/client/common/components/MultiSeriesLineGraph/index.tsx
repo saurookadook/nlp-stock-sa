b8f9d233226d7d1189963eb788ffed0dda25933c
Andy Maskiell <maskiella@gmail.com>
2024-08-03 12:59:59 -0400

    Minor refactors and cleanup to MultiSeriesLineGraph

--- app/src/client/common/components/MultiSeriesLineGraph/index.tsx
+++ app/src/client/common/components/MultiSeriesLineGraph/index.tsx
@@ -1,14 +1,11 @@
 import React, { useEffect, useRef } from 'react';
 import * as d3 from 'd3';
 
-import type { SentimentAnalysesDataEntry } from '@nlpssa-app-types/common/main';
+import type { SentimentAnalysesDataEntry, DispatchParams } from '@nlpssa-app-types/common/main';
 import Legend from 'client/common/components/MultiSeriesLineGraph/Legend';
 import { polarities, strokeColorByPolarity } from 'client/common/components/MultiSeriesLineGraph/constants';
+import { createXScale, createYScale, buildPoints } from 'client/common/components/MultiSeriesLineGraph/utils';
 import { StyledGraphWrapper, StyledSVG } from 'client/common/components/MultiSeriesLineGraph/styled';
-
-type DispatchParams = d3.CustomEventParameters & { bubble?: boolean };
-
-type D3Point = [number, number, string];
 
 // 640 / 16 = 40
 // 400 / 16 = 25
@@ -35,49 +32,27 @@ function MultiSeriesLineGraph({
     marginLeft?: number;
 }>) {
     const svgRef = useRef<SVGSVGElement>(null);
-    const gx = useRef<SVGGElement>(null);
-    const gy = useRef<SVGGElement>(null);
+    const gxRef = useRef<SVGGElement>(null);
+    const gyRef = useRef<SVGGElement>(null);
     const linesPathsRef = useRef<SVGGElement>(null);
-    const dot = useRef<SVGGElement>(null);
+    const dotRef = useRef<SVGGElement>(null);
 
-    const dateValue = (d: SentimentAnalysesDataEntry) =>
-        (d.source?.data?.last_updated_date || d.source?.data?.published_date) as Date;
+    const x = createXScale({
+        data: sentimentAnalysesData,
+        marginLeft,
+        marginRight,
+        width,
+    });
 
-    const x = d3
-        .scaleUtc()
-        .domain(d3.extent(sentimentAnalysesData, dateValue) as [Date, Date])
-        .range([marginLeft, width - marginRight]);
+    const y = createYScale({
+        boundLower: -1,
+        boundUpper: 1,
+        height,
+        marginBottom,
+        marginTop,
+    });
 
-    const y = d3
-        .scaleLinear()
-        .domain([-1, 1])
-        .nice()
-        .range([height - marginBottom, marginTop]);
-
-    const points = sentimentAnalysesData.reduce(function (acc: D3Point[], d: SentimentAnalysesDataEntry) {
-        acc.push([
-            x(d.source!.data!.last_updated_date as Date),
-            y(d.output.compound),
-            `${d.quoteStockSymbol}: Compound score`,
-        ]);
-        acc.push([
-            x(d.source!.data!.last_updated_date as Date),
-            y(d.output.neg),
-            `${d.quoteStockSymbol}: Negative score`,
-        ]);
-        acc.push([
-            x(d.source!.data!.last_updated_date as Date),
-            y(d.output.neu),
-            `${d.quoteStockSymbol}: Neutral score`,
-        ]);
-        acc.push([
-            x(d.source!.data!.last_updated_date as Date),
-            y(d.output.pos),
-            `${d.quoteStockSymbol}: Positive score`,
-        ]);
-
-        return acc;
-    }, []);
+    const points = buildPoints({ data: sentimentAnalysesData, xFn: x, yFn: y });
 
     const groups = d3.rollup(
         points,
@@ -92,61 +67,26 @@ function MultiSeriesLineGraph({
 
     const simpleLineGenerator = d3.line<[number, number]>();
 
-    useEffect(() => {
-        if (linesPathsRef.current != null) {
-            d3.select(linesPathsRef.current)
-                .append('g')
-                .classed('lines-paths', true)
-                .attr('fill', 'none')
-                .attr('stroke-width', 1.5)
-                .attr('stroke-linejoin', 'round')
-                .attr('stroke-linecap', 'round')
-                .selectAll('path')
-                .data(groups.values())
-                .join('path')
-                .each(function (p, j) {
-                    const path = d3.select(this);
-                    const polarity = polarities[j];
-                    console.log({ p, j, path, polarity });
-                    path.classed('line-path', true)
-                        .attr('aria-label', polarity)
-                        .style('stroke', strokeColorByPolarity[polarity])
-                        // .attr('d', lineGenerator as (data) => string);
-                        // @ts-expect-error: testing
-                        .attr('d', simpleLineGenerator);
-                    // debugger;
-                });
-        }
-    }, []);
-
     // When the pointer moves, find the closest point, update the interactive tip, and highlight
     // the corresponding line. Note: we don't actually use Voronoi here, since an exhaustive search
     // is fast enough.
     function pointermoved(event) {
-        // console.groupCollapsed('pointermoved callback - event');
-        console.log(event, points);
-        // console.groupEnd();
         try {
             const [eventX, eventY] = d3.pointer(event);
             const i = Number(d3.leastIndex(points, ([x, y]) => Math.hypot(Number(x) - eventX, Number(y) - eventY)));
             const [x, y, k] = points[i];
-            // TODO: fix weird type error
-            // console.groupCollapsed('pointermoved callback - other vars');
-            console.log({ eventX, eventY, i, x, y, k });
-            // console.groupEnd();
-            const paths = d3.select(svgRef.current).selectAll('path.line-path');
-            // console.log({ paths });
-            paths.each(function (d, i, nodes) {
-                const el = nodes[i] as SVGPathElement;
-                const polarityLabel = el.getAttribute('aria-label') || '';
-                // console.log({ el, polarityForNode: polarities[i], polarityLabel });
-                if (k.indexOf(polarityLabel) > -1) {
-                    d3.select(el).raise();
-                } else {
-                    el.style.stroke = '#dddddd';
-                }
-            });
-            d3.select(dot.current) // force formatting
+            const paths = d3.select(linesPathsRef.current).selectAll('path');
+            paths
+                // @ts-expect-error: Not sure how to type this but it's correct
+                .style('stroke', function ({ z }) {
+                    const pathEl = d3.select(this).node() as SVGPathElement;
+                    const polarityLabel = pathEl.getAttribute('aria-label') || '';
+                    return z === k ? strokeColorByPolarity[polarityLabel] : '#dddddd';
+                })
+                // @ts-expect-error: Not sure how to type this but it's correct
+                .filter(({ z }) => z === k)
+                .raise();
+            d3.select(dotRef.current) // force formatting
                 .attr('transform', `translate(${x},${y})`)
                 .select('text')
                 .text(k);
@@ -154,38 +94,43 @@ function MultiSeriesLineGraph({
                 .property('value', sentimentAnalysesData[i])
                 .dispatch('input', { bubbles: true } as DispatchParams);
         } catch (e) {
-            // console.groupCollapsed('ERROR: pointermoved');
+            console.groupCollapsed("ERROR: encountered in 'pointermoved' callback");
             console.error(e);
-            // console.groupEnd();
+            console.groupEnd();
         }
     }
 
-    function pointerentered(event) {
-        console.groupCollapsed('pointerentered callback');
-        console.log({ event });
-        console.groupEnd();
-        d3.select(linesPathsRef.current).style('mix-blend-mode', null).style('stroke', '#ddd');
-        d3.select(dot.current).attr('display', null);
+    function pointerentered() {
+        try {
+            d3.select(linesPathsRef.current).style('mix-blend-mode', null).style('stroke', '#ddd');
+            d3.select(dotRef.current).attr('display', null);
+        } catch (e) {
+            console.groupCollapsed("ERROR: encountered in 'pointerentered' callback");
+            console.error(e);
+            console.groupEnd();
+        }
     }
 
     function pointerleft() {
-        const paths = d3.select(svgRef.current).selectAll('path.line-path');
-        // console.log({ paths });
-        paths.each(function (d, i, nodes) {
-            const el = nodes[i] as SVGPathElement;
-            const polarityLabel = el.getAttribute('aria-label') || '';
-            // console.log({ el, polarityForNode: polarities[i], polarityLabel });
-            el.style.stroke = strokeColorByPolarity[polarityLabel];
-        });
-        d3.select(dot.current).attr('display', 'none');
-        // TODO: fix weird type error
-        // d3.select(svgRef.current).node().value = null;
-        d3.select(svgRef.current).dispatch('input', { bubbles: true } as DispatchParams);
+        try {
+            const paths = d3.select(linesPathsRef.current).selectAll('path');
+            paths.each(function () {
+                const pathEl = d3.select(this);
+                const polarityLabel = (pathEl.node() as SVGPathElement).getAttribute('aria-label') || '';
+                pathEl.style('stroke', strokeColorByPolarity[polarityLabel]);
+            });
+            d3.select(dotRef.current).attr('display', 'none');
+            d3.select(svgRef.current).dispatch('input', { bubbles: true } as DispatchParams);
+        } catch (e) {
+            console.groupCollapsed("ERROR: encountered in 'pointerleft' callback");
+            console.error(e);
+            console.groupEnd();
+        }
     }
 
     useEffect(() => {
-        if (gx.current != null) {
-            d3.select(gx.current).call(
+        if (gxRef.current != null) {
+            d3.select(gxRef.current).call(
                 d3
                     .axisBottom(x)
                     .ticks(width / 80)
@@ -195,8 +140,8 @@ function MultiSeriesLineGraph({
     }, []);
 
     useEffect(() => {
-        if (gy.current != null) {
-            d3.select(gy.current)
+        if (gyRef.current != null) {
+            d3.select(gyRef.current)
                 .call(d3.axisLeft(y))
                 .call((g) => g.select('.domain').remove())
                 .call((g) =>
@@ -218,6 +163,24 @@ function MultiSeriesLineGraph({
         }
     }, []);
 
+    useEffect(() => {
+        if (linesPathsRef.current != null) {
+            d3.select(linesPathsRef.current)
+                .selectAll('path')
+                .data(groups.values())
+                .join('path')
+                .each(function (p, j) {
+                    const path = d3.select(this);
+                    const polarity = polarities[j];
+                    console.log({ p, j, path, polarity });
+                    path.classed('line-path', true)
+                        .attr('aria-label', polarity)
+                        .style('stroke', strokeColorByPolarity[polarity])
+                        .attr('d', simpleLineGenerator as (data) => string);
+                });
+        }
+    }, []);
+
     return (
         <StyledGraphWrapper>
             <StyledSVG
@@ -230,10 +193,17 @@ function MultiSeriesLineGraph({
                 onPointerLeave={pointerleft}
                 onTouchStart={(event) => event.preventDefault()}
             >
-                <g ref={gx} transform={`translate(0,${height - marginBottom})`} />
-                <g ref={gy} transform={`translate(${marginLeft},0)`} />
-                <g ref={linesPathsRef} className="lines-paths" />
-                <g ref={dot} display="none" fill="white" stroke="currentColor" strokeWidth="1.5">
+                <g ref={gxRef} transform={`translate(0, ${height - marginBottom})`} />
+                <g ref={gyRef} transform={`translate(${marginLeft}, 0)`} />
+                <g
+                    ref={linesPathsRef}
+                    className="lines-paths"
+                    fill="none"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={1.5}
+                />
+                <g ref={dotRef} display="none" fill="white" stroke="currentColor" strokeWidth="1.5">
                     <circle r="2.5" />
                     <text textAnchor="middle" y={-8} r="2.5" />
                 </g>
