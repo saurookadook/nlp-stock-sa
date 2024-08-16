32ee77b33f5b43193b8b8e61da4fcb30988219eb
Andy Maskiell <maskiella@gmail.com>
2024-08-16 17:37:41 -0400

    Add Legend to components exports

--- app/src/client/common/components/MultiSeriesLineGraph/components/index.ts
+++ app/src/client/common/components/MultiSeriesLineGraph/components/index.ts
@@ -1,6 +1,7 @@
 import AxisX from './AxisX';
 import AxisY from './AxisY';
 import Graph from './Graph';
+import Legend from './Legend';
 import MultiSeriesLines from './MultiSeriesLines';
 
-export { AxisX, AxisY, Graph, MultiSeriesLines };
+export { AxisX, AxisY, Graph, Legend, MultiSeriesLines };
