9c2543cb36f55f799694b8946728acbba32d14f2
Andy Maskiell <maskiella@gmail.com>
2024-08-16 17:44:30 -0400

    Refactor Axis components to use state context

--- app/src/client/common/components/MultiSeriesLineGraph/components/AxisX/index.tsx
+++ app/src/client/common/components/MultiSeriesLineGraph/components/AxisX/index.tsx
@@ -1,21 +1,28 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useContext, useEffect, useRef } from 'react';
 import * as d3 from 'd3';
 
+import { MSLGraphContext } from 'client/common/components/MultiSeriesLineGraph/constants';
+
 function AxisX() {
+    const stateContext = useContext(MSLGraphContext);
+    const {
+        graphConfig: { height, margins, width, xScale },
+    } = stateContext;
+
     const gxRef = useRef<SVGGElement>(null);
 
     useEffect(() => {
         if (gxRef.current != null) {
             d3.select(gxRef.current).call(
                 d3
-                    .axisBottom(x)
+                    .axisBottom(xScale)
                     .ticks(width / 80)
                     .tickSizeOuter(0),
             );
         }
     }, []);
 
-    return <g ref={gxRef} transform={`translate(0, ${height - marginBottom})`} />;
+    return <g ref={gxRef} transform={`translate(0, ${height - margins.bottom})`} />;
 }
 
 export default AxisX;
--- app/src/client/common/components/MultiSeriesLineGraph/components/AxisY/index.tsx
+++ app/src/client/common/components/MultiSeriesLineGraph/components/AxisY/index.tsx
@@ -1,25 +1,32 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useContext, useEffect, useRef } from 'react';
 import * as d3 from 'd3';
 
+import { MSLGraphContext } from 'client/common/components/MultiSeriesLineGraph/constants';
+
 function AxisY() {
+    const stateContext = useContext(MSLGraphContext);
+    const {
+        graphConfig: { margins, width, yScale },
+    } = stateContext;
+
     const gyRef = useRef<SVGGElement>(null);
 
     useEffect(() => {
         if (gyRef.current != null) {
             d3.select(gyRef.current)
-                .call(d3.axisLeft(y))
+                .call(d3.axisLeft(yScale))
                 .call((g) => g.select('.domain').remove())
                 .call((g) =>
                     g
                         .selectAll('.tick line')
                         .clone()
-                        .attr('x2', width - marginLeft - marginRight)
+                        .attr('x2', width - margins.left - margins.right)
                         .attr('stroke-opacity', 0.1),
                 )
                 .call((g) =>
                     g
                         .append('text')
-                        .attr('x', -marginLeft)
+                        .attr('x', -margins.left)
                         .attr('y', 10)
                         .attr('fill', 'currentColor')
                         .attr('text-anchor', 'start')
@@ -28,7 +35,7 @@ function AxisY() {
         }
     }, []);
 
-    return <g ref={gyRef} transform={`translate(${marginLeft}, 0)`} />;
+    return <g ref={gyRef} transform={`translate(${margins.left}, 0)`} />;
 }
 
 export default AxisY;
