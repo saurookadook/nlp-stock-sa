e0d50a15edd5d24a253947db3407c92b6cd6d319
Andy Maskiell <maskiella@gmail.com>
2024-09-15 21:15:37 -0400

    Add missing null check filter to get_all_sentiment_analyses_by_stock_slug route handler

--- server/nlp_ssa/api/routes/api/sentiment_analyses/route_handlers.py
+++ server/nlp_ssa/api/routes/api/sentiment_analyses/route_handlers.py
@@ -1,11 +1,23 @@
 from models.sentiment_analysis import SentimentAnalysisFacade
 
 
+def filter_func(sa_row):
+    return (
+        sa_row.source.data is not None
+        and sa_row.source.data.last_updated_date is not None
+    )
+
+
 def get_all_sentiment_analyses_by_stock_slug(db_session, stock_slug):
     sentiment_analyses_rows = SentimentAnalysisFacade(
         db_session=db_session
     ).get_all_by_stock_symbol(quote_stock_symbol=stock_slug)
 
+    filtered_sentiment_analyses_rows = list(
+        filter(filter_func, sentiment_analyses_rows)
+    )
+
     return sorted(
-        sentiment_analyses_rows, key=lambda sa: sa.source.data.last_updated_date
+        filtered_sentiment_analyses_rows,
+        key=lambda sa: sa.source.data.last_updated_date,
     )
