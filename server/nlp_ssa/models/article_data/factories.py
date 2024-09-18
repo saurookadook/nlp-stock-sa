8169221d55e2255098e96d3641216744ded76bc7
Andy Maskiell <maskiella@gmail.com>
2024-09-17 22:48:17 -0400

    Fix SentimentAnalysisFactory and ArticleDataFactory, fix ArticleDataFacade tests

--- server/nlp_ssa/models/article_data/factories.py
+++ server/nlp_ssa/models/article_data/factories.py
@@ -7,6 +7,7 @@ from models.mixins.factories import (
     OwnedByPolymorphicSourceFactory,
     TimestampsMixinFactory,
 )
+from utils.testing_mocks import get_mock_utcnow
 
 
 class ArticleDataFactory(
@@ -19,12 +20,14 @@ class ArticleDataFactory(
         sqlalchemy_session = db_session
 
     id = factory.LazyFunction(lambda: uuid4())
+    last_updated_date = factory.LazyFunction(lambda: get_mock_utcnow())
+    published_date = factory.LazyFunction(lambda: get_mock_utcnow().shift(months=-6))
     quote_stock_symbol = factory.Transformer(
         factory.Faker("random_letters", length=6),
         transform=lambda o: "".join(o).upper(),
     )
-    source_group_id = factory.LazyFunction(lambda: uuid4())
-    source_url = factory.Faker("url")
     raw_content = factory.Faker("text", max_nb_chars=5000)
     # TODO: make this clean text like in the scrapers
     sentence_tokens = factory.LazyAttribute(lambda ad: ad.raw_content)
+    source_group_id = factory.LazyFunction(lambda: uuid4())
+    source_url = factory.Faker("url")
