1090ae18bd6fb8a009255c49ef9bb04c530c65ad
Andy Maskiell <maskiella@gmail.com>
2025-09-08 14:14:31 -0400

    feat: minor update to analysis_views test route

--- server/nlp_ssa/api/routes/api/analysis_views/router.py
+++ server/nlp_ssa/api/routes/api/analysis_views/router.py
@@ -1,7 +1,9 @@
 import logging
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy import select
 
+from api.dependencies.user_session import handle_user_session
+from api.routes.auth.session.models import UserSessionCacheValue
 from config.logging import ExtendedLogger
 from db import db_session
 
@@ -11,17 +13,28 @@ router = APIRouter()
 
 
 @router.get("/api/analysis-views/test")
-async def read_analysis_views():
+async def read_analysis_views(
+    user_session: UserSessionCacheValue = Depends(handle_user_session),
+):
     from models.analysis_view import AnalysisViewDB
     from models.user import UserFacade
     from models.sentiment_analysis import SentimentAnalysisDB
 
+    # TODO: START -------------------------------------------------------------
+    # - this could/should all probably go into a dependency
+    if not user_session:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Current user not authorized to access this resource.",
+        )
+
     user_facade = UserFacade(db_session=db_session)
 
     try:
-        user = user_facade.get_one_by_username("ovalle15")
+        user = user_facade.get_one_by_session_id(user_session.user_session_id)
     except Exception as e:
         raise e
+    # TODO: END ---------------------------------------------------------------
 
     analysis_views = (
         db_session.execute(
@@ -46,6 +59,7 @@ async def read_analysis_views():
 
     results = []
 
+    # TODO: this almost certainly isn't going to work as intended lol
     for tuple_result in zip(analysis_views, sentiment_analyses):
         results.append(
             dict(analysis_view=tuple_result[0], sentiment_analysis=tuple_result[1])
