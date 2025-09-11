83998d92f5bbbb0606687c060e2d58bd2b698e82
Andy Maskiell <maskiella@gmail.com>
2025-09-11 16:02:28 -0400

    chore: minor clean up, minor improvements to stories

--- app/src/client/common/components/ArticleDataList/index.tsx
+++ app/src/client/common/components/ArticleDataList/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import {
-    Accordion, // force formatting
-    AccordionItem,
-    AccordionButton,
-    AccordionPanel,
-    AccordionIcon,
-    Box,
-    Flex,
-    Heading,
-    SimpleGrid,
-    Spacer,
-    Text,
+  Accordion, // force formatting
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  Box,
+  Flex,
+  Heading,
+  SimpleGrid,
+  Spacer,
+  Text,
 } from '@chakra-ui/react';
 
 import type { ArticleDataEntry } from '@nlpssa-app-types/common/main';
@@ -18,120 +18,146 @@ import { BaseLink } from 'client/common/components';
 import { toTitleCase } from 'client/common/utils';
 
 function getTitleWithFallback(articleRecordArg) {
-    return (
-        articleRecordArg.title ||
-        articleRecordArg.sourceUrl
-            .replace(/^https:\/\/finance.yahoo.com\/(news\/|m\/[^\/]+?[\/])|\.html$/gim, '')
-            .replace(/-/g, ' ')
-            .toUpperCase()
-    );
+  return (
+    articleRecordArg.title ||
+    articleRecordArg.sourceUrl
+      .replace(/^https:\/\/finance.yahoo.com\/(news\/|m\/[^\/]+?[\/])|\.html$/gim, '')
+      .replace(/-/g, ' ')
+      .toUpperCase()
+  );
 }
 
 function getMetadataLabelFromKey(metadataKey: string) {
-    const key = metadataKey.replace(/Date$/i, '');
-    return toTitleCase(key);
+  const key = metadataKey.replace(/Date$/i, '');
+  return toTitleCase(key);
 }
 
-function ArticleAccordianHeading({ articleRecord }: React.PropsWithChildren<{ articleRecord: ArticleDataEntry }>) {
-    return (
-        <Heading>
-            <AccordionButton>
-                <Box as="span" fontWeight="700" display="inline-flex" textAlign="left" whiteSpace="pre-wrap">
-                    {getTitleWithFallback(articleRecord)}
-                </Box>
-                <Spacer />
-                <AccordionIcon />
-            </AccordionButton>
-        </Heading>
-    );
+function ArticleAccordianHeading({
+  articleRecord,
+}: React.PropsWithChildren<{ articleRecord: ArticleDataEntry }>) {
+  return (
+    <Heading>
+      <AccordionButton>
+        <Box
+          as="span"
+          fontWeight="700"
+          display="inline-flex"
+          textAlign="left"
+          whiteSpace="pre-wrap"
+        >
+          {getTitleWithFallback(articleRecord)}
+        </Box>
+
+        <Spacer />
+
+        <AccordionIcon />
+      </AccordionButton>
+    </Heading>
+  );
 }
 
 function ArticleMetadata({ articleRecord }) {
-    const metadataKeys = Object.keys(articleRecord).filter(
-        (key) => !/polymorphicSource|rawContent|sentenceTokens|sourceUrl/im.test(key),
-    );
+  const metadataKeys = Object.keys(articleRecord).filter(
+    (key) => !/polymorphicSource|rawContent|sentenceTokens|sourceUrl/im.test(key),
+  );
 
-    return (
-        <SimpleGrid
-            columns={2}
-            borderBottom="1px solid"
-            borderColor="gray.400"
-            marginBottom="1rem"
-            paddingBottom="1rem"
-        >
-            {metadataKeys.map((key, i) => {
-                return (
-                    articleRecord[key] && (
-                        <Box key={`${key}-${i}`}>
-                            <b>{getMetadataLabelFromKey(key)}</b>: {articleRecord[key]}
-                        </Box>
-                    )
-                );
-            })}
-            <Box>
-                <b>URL</b>
-                {': '}
-                <BaseLink color="teal" href={articleRecord.sourceUrl} isExternal rel="noopener noreferrer">
-                    {articleRecord.sourceUrl}
-                </BaseLink>
+  return (
+    <SimpleGrid
+      columns={2}
+      borderBottom="1px solid"
+      borderColor="gray.400"
+      marginBottom="1rem"
+      paddingBottom="1rem"
+    >
+      {metadataKeys.map((key, i) => {
+        return (
+          articleRecord[key] && (
+            <Box key={`${key}-${i}`}>
+              <b>{getMetadataLabelFromKey(key)}</b>: {articleRecord[key]}
             </Box>
-        </SimpleGrid>
-    );
+          )
+        );
+      })}
+
+      <Box>
+        <b>URL</b>
+        {': '}
+        <BaseLink
+          color="teal"
+          href={articleRecord.sourceUrl}
+          isExternal
+          rel="noopener noreferrer"
+        >
+          {articleRecord.sourceUrl}
+        </BaseLink>
+      </Box>
+    </SimpleGrid>
+  );
 }
 
 function ArticleAccordianPanel({ articleRecord }) {
-    return (
-        <AccordionPanel>
-            <Flex flexDirection="column" justifyContent="center">
-                <ArticleMetadata articleRecord={articleRecord} />
-            </Flex>
-            <Flex // force formatting
-                alignItems="flex-start"
-                flexDirection="row"
-                justifyContent="space-evenly"
-                width="100%"
-            >
-                <Box // force formatting
-                    className="raw-content"
-                    display="inline-flex"
-                    flexDirection="column"
-                    maxWidth="45%"
-                >
-                    <h2 style={{ fontWeight: 'bold' }}>Raw Content</h2>
-                    <Text>{articleRecord.rawContent}</Text>
-                </Box>
-                <Box // force formatting
-                    className="sentence-tokens"
-                    display="inline-flex"
-                    flexDirection="column"
-                    maxWidth="45%"
-                >
-                    <h2 style={{ fontWeight: 'bold' }}>Sentence Tokens</h2>
-                    <Text>{articleRecord.sentenceTokens}</Text>
-                </Box>
-            </Flex>
-        </AccordionPanel>
-    );
+  return (
+    <AccordionPanel>
+      <Flex flexDirection="column" justifyContent="center">
+        <ArticleMetadata articleRecord={articleRecord} />
+      </Flex>
+
+      <Flex // force formatting
+        alignItems="flex-start"
+        flexDirection="row"
+        justifyContent="space-evenly"
+        width="100%"
+      >
+        <Box // force formatting
+          className="raw-content"
+          display="inline-flex"
+          flexDirection="column"
+          maxWidth="45%"
+        >
+          <h2 style={{ fontWeight: 'bold' }}>Raw Content</h2>
+          <Text>{articleRecord.rawContent}</Text>
+        </Box>
+
+        <Box // force formatting
+          className="sentence-tokens"
+          display="inline-flex"
+          flexDirection="column"
+          maxWidth="45%"
+        >
+          <h2 style={{ fontWeight: 'bold' }}>Sentence Tokens</h2>
+          <Text>{articleRecord.sentenceTokens}</Text>
+        </Box>
+      </Flex>
+    </AccordionPanel>
+  );
 }
 
-function ArticleDataListItem({ articleRecord }: React.PropsWithChildren<{ articleRecord: ArticleDataEntry }>) {
-    return (
-        <AccordionItem>
-            <ArticleAccordianHeading articleRecord={articleRecord} />
-            <ArticleAccordianPanel articleRecord={articleRecord} />
-        </AccordionItem>
-    );
+function ArticleDataListItem({
+  articleRecord,
+}: React.PropsWithChildren<{ articleRecord: ArticleDataEntry }>) {
+  return (
+    <AccordionItem>
+      <ArticleAccordianHeading articleRecord={articleRecord} />
+
+      <ArticleAccordianPanel articleRecord={articleRecord} />
+    </AccordionItem>
+  );
 }
 
-function ArticleDataList({ articleData }: React.PropsWithChildren<{ articleData: ArticleDataEntry[] }>) {
-    return (
-        // TODO: maybe this should use `ul` and `li` elements?
-        <Accordion allowMultiple>
-            {articleData.map((record, i) => (
-                <ArticleDataListItem key={`article-record-${i}`} articleRecord={record} />
-            ))}
-        </Accordion>
-    );
+function ArticleDataList({
+  articleData,
+}: React.PropsWithChildren<{ articleData: ArticleDataEntry[] }>) {
+  return (
+    // TODO: maybe this should use `ul` and `li` elements?
+    <Accordion allowMultiple>
+      {articleData.map((record) => (
+        <ArticleDataListItem
+          key={`article-record-${record.id}`}
+          articleRecord={record}
+        />
+      ))}
+    </Accordion>
+  );
 }
 
 export default ArticleDataList;
--- app/src/client/data-explorers/explorers/article-data/ArticleDataBySlugExplorer/stories.tsx
+++ app/src/client/data-explorers/explorers/article-data/ArticleDataBySlugExplorer/stories.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import type { Meta, StoryObj } from '@storybook/react';
 import {
-    reactRouterOutlets, // force formatting
-    reactRouterParameters,
-    withRouter,
+  reactRouterOutlets, // force formatting
+  reactRouterParameters,
+  withRouter,
 } from 'storybook-addon-remix-react-router';
 
 import { getStoryArticleData } from 'client/_story-data';
@@ -13,19 +13,19 @@ import { dataExplorersRoutes } from 'client/data-explorers/routes';
 import { AppStateProvider } from 'client/data-explorers/store';
 
 const meta = {
-    title: 'Data Explorers/ArticleData/ArticleDataBySlugExplorer',
-    component: ArticleDataBySlugExplorer,
-    decorators: [withRouter],
-    parameters: {
-        // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
-        layout: 'fullscreen',
-        reactRouter: reactRouterParameters({
-            routing: {
-                ...reactRouterOutlets(dataExplorersRoutes),
-                path: '/data-explorers/article-data/:stockSlug',
-            },
-        }),
-    },
+  title: 'Data Explorers/ArticleData/ArticleDataBySlugExplorer',
+  component: ArticleDataBySlugExplorer,
+  decorators: [withRouter],
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
+    layout: 'fullscreen',
+    reactRouter: reactRouterParameters({
+      routing: {
+        ...reactRouterOutlets(dataExplorersRoutes),
+        path: '/data-explorers/article-data/:stockSlug',
+      },
+    }),
+  },
 } satisfies Meta<typeof ArticleDataBySlugExplorer>;
 
 export default meta;
@@ -33,103 +33,73 @@ type Story = StoryObj<typeof meta>;
 
 const articleData = getStoryArticleData().data;
 
+function renderStory() {
+  const params = useParams();
+  console.log({ params });
+
+  const _articleDataBySlug = articleData!.find(
+    (data) => data.quoteStockSymbol === params.stockSlug,
+  );
+
+  return (
+    <AppStateProvider initialState={{ articleDataBySlug: _articleDataBySlug }}>
+      <ArticleDataBySlugExplorer />
+    </AppStateProvider>
+  );
+}
+
 // TODO: is it possible to use controls from Storybook to change the slug value?
 export const BaseArticleDataBySlugExplorerDOOG: Story = {
-    parameters: {
-        reactRouter: reactRouterParameters({
-            location: {
-                pathParams: { stockSlug: 'DOOG' },
-            },
-        }),
-    },
-    render: () => {
-        const params = useParams();
-        console.log({ params });
-        const dataBySlug = articleData!.find((data) => data.quoteStockSymbol === params.stockSlug);
-        return (
-            <AppStateProvider initialState={{ articleDataBySlug: dataBySlug }}>
-                <ArticleDataBySlugExplorer />
-            </AppStateProvider>
-        );
-    },
+  parameters: {
+    reactRouter: reactRouterParameters({
+      location: {
+        pathParams: { stockSlug: 'DOOG' },
+      },
+    }),
+  },
+  render: renderStory,
 };
 
 export const BaseArticleDataBySlugExplorerINDO: Story = {
-    parameters: {
-        reactRouter: reactRouterParameters({
-            location: {
-                pathParams: { stockSlug: 'INDO' },
-            },
-        }),
-    },
-    render: () => {
-        const params = useParams();
-        console.log({ params });
-        const dataBySlug = articleData!.find((data) => data.quoteStockSymbol === params.stockSlug);
-        return (
-            <AppStateProvider initialState={{ articleDataBySlug: dataBySlug }}>
-                <ArticleDataBySlugExplorer />
-            </AppStateProvider>
-        );
-    },
+  parameters: {
+    reactRouter: reactRouterParameters({
+      location: {
+        pathParams: { stockSlug: 'INDO' },
+      },
+    }),
+  },
+  render: renderStory,
 };
 
 export const BaseArticleDataBySlugExplorerKOS: Story = {
-    parameters: {
-        reactRouter: reactRouterParameters({
-            location: {
-                pathParams: { stockSlug: 'KOS' },
-            },
-        }),
-    },
-    render: () => {
-        const params = useParams();
-        console.log({ params });
-        const dataBySlug = articleData!.find((data) => data.quoteStockSymbol === params.stockSlug);
-        return (
-            <AppStateProvider initialState={{ articleDataBySlug: dataBySlug }}>
-                <ArticleDataBySlugExplorer />
-            </AppStateProvider>
-        );
-    },
+  parameters: {
+    reactRouter: reactRouterParameters({
+      location: {
+        pathParams: { stockSlug: 'KOS' },
+      },
+    }),
+  },
+  render: renderStory,
 };
 
 export const BaseArticleDataBySlugExplorerNTDOF: Story = {
-    parameters: {
-        reactRouter: reactRouterParameters({
-            location: {
-                pathParams: { stockSlug: 'NTDOF' },
-            },
-        }),
-    },
-    render: () => {
-        const params = useParams();
-        console.log({ params });
-        const dataBySlug = articleData!.find((data) => data.quoteStockSymbol === params.stockSlug);
-        return (
-            <AppStateProvider initialState={{ articleDataBySlug: dataBySlug }}>
-                <ArticleDataBySlugExplorer />
-            </AppStateProvider>
-        );
-    },
+  parameters: {
+    reactRouter: reactRouterParameters({
+      location: {
+        pathParams: { stockSlug: 'NTDOF' },
+      },
+    }),
+  },
+  render: renderStory,
 };
 
 export const BaseArticleDataBySlugExplorerTSLA: Story = {
-    parameters: {
-        reactRouter: reactRouterParameters({
-            location: {
-                pathParams: { stockSlug: 'TSLA' },
-            },
-        }),
-    },
-    render: () => {
-        const params = useParams();
-        console.log({ params });
-        const dataBySlug = articleData!.find((data) => data.quoteStockSymbol === params.stockSlug);
-        return (
-            <AppStateProvider initialState={{ articleDataBySlug: dataBySlug }}>
-                <ArticleDataBySlugExplorer />
-            </AppStateProvider>
-        );
-    },
+  parameters: {
+    reactRouter: reactRouterParameters({
+      location: {
+        pathParams: { stockSlug: 'TSLA' },
+      },
+    }),
+  },
+  render: renderStory,
 };
