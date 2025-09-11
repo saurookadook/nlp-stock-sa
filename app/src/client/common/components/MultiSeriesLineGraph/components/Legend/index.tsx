ab13e77fcfcaea3890f7d5ebfb2b97a442ada0ac
Andy Maskiell <maskiella@gmail.com>
2025-09-11 15:19:19 -0400

    feat: minor update to Legend component

--- app/src/client/common/components/MultiSeriesLineGraph/components/Legend/index.tsx
+++ app/src/client/common/components/MultiSeriesLineGraph/components/Legend/index.tsx
@@ -1,39 +1,42 @@
 import React, { useContext } from 'react';
 
-import { strokeColorByPolarity, MSLGraphContext } from 'client/common/components/MultiSeriesLineGraph/constants';
+import {
+  strokeColorByPolarity,
+  MSLGraphContext,
+} from 'client/common/components/MultiSeriesLineGraph/constants';
 
 function Legend() {
-    const stateContext = useContext(MSLGraphContext);
-    const {
-        graphConfig: { height, legend },
-    } = stateContext;
+  const stateContext = useContext(MSLGraphContext);
+  const {
+    graphConfig: { height, legend },
+  } = stateContext;
 
-    return (
-        <g id="legend">
-            {Object.entries(strokeColorByPolarity).map(([key, val], i) => {
-                return (
-                    <React.Fragment key={`legend-item-${i}`}>
-                        <rect
-                            x={100 + i * 100}
-                            y={height - legend.itemSize + legend.spacer}
-                            fill={val}
-                            height={legend.itemSize}
-                            width={legend.itemSize}
-                        />
-                        <text
-                            x={100 + i * 100 + legend.itemSize + 5}
-                            y={height + legend.spacer / 2}
-                            alignmentBaseline="middle"
-                            fill={val}
-                            textAnchor="left"
-                        >
-                            {key}
-                        </text>
-                    </React.Fragment>
-                );
-            })}
-        </g>
-    );
+  return (
+    <g id="legend">
+      {Object.entries(strokeColorByPolarity).map(([key, colorConfig], i) => {
+        return (
+          <React.Fragment key={`legend-item-${i}`}>
+            <rect
+              x={100 + i * 100}
+              y={height - legend.itemSize + legend.spacer}
+              fill={colorConfig.hex}
+              height={legend.itemSize}
+              width={legend.itemSize}
+            />
+            <text
+              x={100 + i * 100 + legend.itemSize + 5}
+              y={height + legend.spacer / 2}
+              alignmentBaseline="middle"
+              fill={colorConfig.hex}
+              textAnchor="left"
+            >
+              {key}
+            </text>
+          </React.Fragment>
+        );
+      })}
+    </g>
+  );
 }
 
 export default Legend;
