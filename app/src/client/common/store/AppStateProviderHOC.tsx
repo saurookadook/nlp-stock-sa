57cd863dea4d5e2cc2dd8ab13092762035dc644d
Andy Maskiell <maskiella@gmail.com>
2025-09-13 14:22:44 -0400

    feat: refactor AppStateProviderHOC to accept StateContext and DispatchContext

--- app/src/client/common/store/AppStateProviderHOC.tsx
+++ app/src/client/common/store/AppStateProviderHOC.tsx
@@ -1,35 +1,39 @@
 import React, { useReducer } from 'react';
 
 import { CombinedStateSliceReducer } from '@nlpssa-app-types/common/main';
-import { BaseStateContext, BaseDispatchContext } from 'client/common/store/contexts';
 import { deeplyMerge } from 'client/common/utils';
 
-function AppStateProviderHOC<InitialState = any>(
-    combinedReducer: CombinedStateSliceReducer,
-) {
-    return function AppStateProvider({
-        children, // force formatting
-        initialState,
-    }: {
-        children: React.ReactElement;
-        initialState: InitialState;
-    }) {
-        const [combinedReducerFunc, combinedDefaultState] = combinedReducer;
+function AppStateProviderHOC<
+  State = any, // force formatting
+  ReducerAction = any,
+  InitialState = any,
+>({
+  StateContext,
+  DispatchContext,
+  combinedReducer,
+}: {
+  StateContext: React.Context<State>;
+  DispatchContext: React.Context<React.Dispatch<ReducerAction>>;
+  combinedReducer: CombinedStateSliceReducer;
+}) {
+  return function AppStateProvider({
+    children, // force formatting
+    initialState,
+  }: {
+    children: React.ReactElement;
+    initialState: InitialState;
+  }) {
+    const [combinedReducerFunc, combinedDefaultState] = combinedReducer;
 
-        const recursivelyMergedState = deeplyMerge(combinedDefaultState, initialState);
-        const [state, dispatch] = useReducer(
-            combinedReducerFunc,
-            recursivelyMergedState,
-        );
+    const recursivelyMergedState = deeplyMerge(combinedDefaultState, initialState);
+    const [state, dispatch] = useReducer(combinedReducerFunc, recursivelyMergedState);
 
-        return (
-            <BaseStateContext.Provider value={state}>
-                <BaseDispatchContext.Provider value={dispatch}>
-                    {children}
-                </BaseDispatchContext.Provider>
-            </BaseStateContext.Provider>
-        );
-    };
+    return (
+      <StateContext.Provider value={state}>
+        <DispatchContext.Provider value={dispatch}>{children}</DispatchContext.Provider>
+      </StateContext.Provider>
+    );
+  };
 }
 
 export default AppStateProviderHOC;
