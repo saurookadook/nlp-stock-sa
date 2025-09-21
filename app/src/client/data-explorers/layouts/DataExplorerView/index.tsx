388d494120634491f1a4942517d5003ce8ec0b0e
Andy Maskiell <maskiella@gmail.com>
2025-09-21 17:23:48 -0400

    feat: add dynamic class name to DataExplorerView layout component

--- app/src/client/data-explorers/layouts/DataExplorerView/index.tsx
+++ app/src/client/data-explorers/layouts/DataExplorerView/index.tsx
@@ -1,10 +1,40 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useMemo } from 'react';
+import { Outlet, useLocation, useParams } from 'react-router-dom';
 import { Center, Container } from '@chakra-ui/react';
 
 function DataExplorerView() {
+  const location = useLocation();
+  const params = useParams();
+
+  console.log({
+    location,
+    params,
+  });
+
+  const dataExplorerViewType = useMemo(() => {
+    const dataEntity = (location.pathname.match(
+      /(?<=(\/app)?\/data-explorers\/)[^\/]+?(?=\/|$)/gim,
+    ) || [])[0];
+    const { stockSlug } = params;
+
+    return stockSlug != null // force formatting
+      ? `${dataEntity}-by-slug`
+      : dataEntity;
+  }, [location, params]);
+
+  const containerClassName = useMemo(() => {
+    return ['data-explorer-view', dataExplorerViewType].join(' ');
+  }, [dataExplorerViewType]);
+
   return (
-    <Container className="data-explorer-view" margin="0 auto" maxWidth="75vw">
+    <Container
+      className={containerClassName}
+      margin="0 auto"
+      maxWidth={
+        // TODO: this feels dirty lol
+        dataExplorerViewType === 'sentiment-analyses-by-slug' ? '100vw' : '75vw'
+      }
+    >
       <Center
         borderColor="gray.400"
         borderRadius="5px"
